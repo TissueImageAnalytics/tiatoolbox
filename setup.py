eb4a99061ea290b5d64767ad7093911a770033ff
shaneahmed <sa>
2020-06-01 15:42:42 +0100

    BUG: Travis error fix setuptools Add setuptools 45.1.0 to make it compatible with openslide-python Add opencv-python to latest version 4.2.0.34 as 3.3.1.11 is not available for python 3.7 & 3.8

--- setup.py
+++ setup.py
@@ -25,13 +25,12 @@ test_requirements = [
 setup(
     author="TIA Lab",
     author_email="tialab@dcs.warwick.ac.uk",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
