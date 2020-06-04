3d9f6e47b12c7d160b3a1a612a8b0a1088f02bea
shaneahmed <sa>
2020-06-04 23:34:37 +0100

    TST: Modify tests to using setup/teardown functions Update pytest fixtures in test_tiatoolbox.py to  use setup/teardown functions https://docs.pytest.org/en/latest/fixture.html#fixture-finalization-executing-teardown-code

--- tests/test_tiatoolbox.py
+++ tests/test_tiatoolbox.py
@@ -15,31 +15,47 @@ import pathlib
 
 
 @pytest.fixture
-def response_ndpi():
+def response_ndpi(request):
     """
     Sample pytest fixture for ndpi images
     Download ndpi image for pytest
     """
-    if not os.path.isfile("./CMU-1.ndpi"):
+    ndpi_file_path = pathlib.Path(__file__).parent.joinpath("CMU-1.ndpi")
+    if not pathlib.Path.is_file(ndpi_file_path):
         r = requests.get(
             "http://openslide.cs.cmu.edu/download/openslide-testdata/Hamamatsu/CMU-1.ndpi"
         )
-        with open("./CMU-1.ndpi", "wb") as f:
+        with open(ndpi_file_path, "wb") as f:
             f.write(r.content)
+
+    def close_ndpi():
+        if pathlib.Path.is_file(ndpi_file_path):
+            os.remove(str(ndpi_file_path))
+
+    request.addfinalizer(close_ndpi)
+    return response_ndpi
 
 
 @pytest.fixture
-def response_svs():
+def response_svs(request):
     """
     Sample pytest fixture for svs images
     Download ndpi image for pytest
     """
-    if not os.path.isfile("./CMU-1.svs"):
+    svs_file_path = pathlib.Path(__file__).parent.joinpath("CMU-1.svs")
+    if not pathlib.Path.is_file(svs_file_path):
         r = requests.get(
-            "http://openslide.cs.cmu.edu/download/openslide-testdata/Aperio/CMU-1.svs"
+            "http://openslide.cs.cmu.edu/download/openslide-testdata/Hamamatsu/CMU-1.ndpi"
         )
-        with open("./CMU-1.svs", "wb") as f:
+        with open(svs_file_path, "wb") as f:
             f.write(r.content)
+
+    def close_ndpi():
+        if pathlib.Path.is_file(svs_file_path):
+            os.remove(str(svs_file_path))
+
+    request.addfinalizer(close_ndpi)
+    return response_svs
 
 
 def test_slide_info(response_ndpi, response_svs):
