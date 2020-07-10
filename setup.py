bceed97e284c5990a6a723948515e3ccfe38fb09
John Pocock <j.c.pocock@warwick.ac.uk>
2020-07-10 15:58:10 +0100

    REL: Fix Version Number Strings
    - Change version strings in setup.py and tiatoolbox/__init__.py
    - Change bum2version rules in setup.cfg for look for double quotes

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tialab/tiatoolbox",
-    version="version='0.2.0'",
+    version="0.2.0",
     zip_safe=False,
 )
