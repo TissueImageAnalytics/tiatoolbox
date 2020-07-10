09fe38dee6a5f7db6e92953610fe53731dec3b76
John Pocock <j.c.pocock@warwick.ac.uk>
2020-07-10 14:58:18 +0100

    DOC: Change RST Files to MD
    - Change files shared between GitHub/PyPI and Sphinx/ReadTheDocs from
    ReStructured Test to Markdown. These are readme, histroy, authors.
    - Add recommonmark in new requiremenst.dev.conda.yml file
    - Add symlinks in docs/ to root readme files for reade, authors,
    history.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,12 @@ import tiatoolbox
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "recommonmark",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -41,7 +46,11 @@ templates_path = ["_templates"]
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
 
 # The master toctree document.
 master_doc = "index"
