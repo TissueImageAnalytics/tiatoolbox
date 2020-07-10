7ccd06a94ed5254c13ecff4aedb33b3313a2b719
John Pocock <j.c.pocock@warwick.ac.uk>
2020-07-10 15:14:39 +0100

    REL: Add Setup.py Description Content Type
    - Change setup.py to use .md files instead of .rst
    - Add long_description_content_type

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 
 from setuptools import setup, find_packages
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
 requirements = [
@@ -47,6 +47,7 @@ setup(
     entry_points={"console_scripts": ["tiatoolbox=tiatoolbox.cli:main", ], },
     install_requires=requirements,
     long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="tiatoolbox",
     name="tiatoolbox",
