3abdec3850250689f7e596f6aa713ea9dcc8fdd0
shaneahmed <sa>
2020-07-12 11:30:56 +0100

    Bump version: 0.2.1 → 0.2.2

--- tiatoolbox/__init__.py
+++ tiatoolbox/__init__.py
@@ -6,7 +6,7 @@ from tiatoolbox import utils
 
 __author__ = """TIA Lab"""
 __email__ = "tialab@dcs.warwick.ac.uk"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 if __name__ == "__main__":
     pass
