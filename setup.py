0283e1eaa017dbb66825d638eb201d770cfc3955
shaneahmed <sa>
2020-07-18 23:50:05 +0100

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tialab/tiatoolbox",
-    version="0.2.2",
+    version="0.3.0",
     zip_safe=False,
 )
