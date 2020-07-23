e1f096bd6b1b2a0dde3b0bfa6fab991e12cfa775
shaneahmed <sa>
2020-07-23 20:07:04 +0100

    TST: Update slide_info.py to improve codecov Update slide_info.py to improve codecov

--- tiatoolbox/dataloader/slide_info.py
+++ tiatoolbox/dataloader/slide_info.py
@@ -5,6 +5,49 @@ from tiatoolbox.decorators.multiproc import TIAMultiProcess
 import os
 
 
+def slide_info_single(input_path, output_dir=None, verbose=True):
+    """Wrapper for multi core slide_info
+    Args:
+        input_path (str): Path to whole slide image
+        output_dir (str): Path to output directory to save the output
+        verbose(bool): Print output, default=True
+
+    Returns:
+        dict: dictionary with Whole Slide meta information
+
+    Examples:
+        >>> from tiatoolbox.dataloader.slide_info import slide_info
+        >>> from tiatoolbox import utils
+        >>> file_types = ("*.ndpi", "*.svs", "*.mrxs")
+        >>> files_all = utils.misc.grab_files_from_dir(input_path,
+        ...     file_types=file_types)
+        >>> for i in range(len(files_all)):
+        ...     slide_param = slide_info_single(input_path=files_all[i])
+        ...     utils.misc.save_yaml(slide_param,
+        ...             slide_param["file_name"] + ".yaml")
+        ...        print(slide_param)
+
+    """
+    input_dir, file_name = os.path.split(input_path)
+
+    if verbose:
+        print(file_name, flush=True)
+    _, file_type = os.path.splitext(file_name)
+
+    if file_type in (".svs", ".ndpi", ".mrxs"):
+        wsi_reader = wsireader.OpenSlideWSIReader(
+            input_dir=input_dir, file_name=file_name, output_dir=output_dir
+        )
+        info = wsi_reader.slide_info
+        if verbose:
+            print(info)
+    else:
+        print("File type not supported")
+        info = None
+
+    return info
+
+
 @TIAMultiProcess(iter_on="input_path")
 def slide_info(input_path, output_dir=None, verbose=True):
     """Single file run to output or save WSI meta data.
@@ -31,23 +74,7 @@ def slide_info(input_path, output_dir=None, verbose=True):
         ...             slide_param["file_name"] + ".yaml")
         ...        print(slide_param)
 
-    """
-
-    input_dir, file_name = os.path.split(input_path)
-
-    if verbose:
-        print(file_name, flush=True)
-    _, file_type = os.path.splitext(file_name)
-
-    if file_type in (".svs", ".ndpi", ".mrxs"):
-        wsi_reader = wsireader.OpenSlideWSIReader(
-            input_dir=input_dir, file_name=file_name, output_dir=output_dir
-        )
-        info = wsi_reader.slide_info
-        if verbose:
-            print(info)
-    else:
-        print("File type not supported")
-        info = None
-
-    return info
+        """
+    return slide_info_single(
+        input_path=input_path, output_dir=output_dir, verbose=verbose
+    )
