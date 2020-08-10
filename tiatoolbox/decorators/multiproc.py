0090f3dc82c8922a838d57a9d55c459745f9cbb7
shaneahmed <sa>
2020-08-10 12:00:55 +0100

    REV: Revert an earlier commit for multiproc.py due to travis issues. Revert an earlier commit for multiproc.py due to travis issues.

--- tiatoolbox/decorators/multiproc.py
+++ tiatoolbox/decorators/multiproc.py
@@ -61,8 +61,6 @@ class TIAMultiProcess:
 
             with Pool(self.workers) as p:
                 results = p.map(partial(func, **kwargs), iter_value,)
-                p.terminate()
-                p.restart()
                 p.clear()
 
             return results
