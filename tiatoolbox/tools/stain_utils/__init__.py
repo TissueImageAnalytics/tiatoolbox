6ea2b9d209909e8a3f941f6ab8533d235c2ac8dc
Simon Graham <simongraham@MacBook-Pro.local>
2020-09-16 17:50:28 +0100

    BUG: Fix import in init

--- tiatoolbox/tools/stain_utils/__init__.py
+++ tiatoolbox/tools/stain_utils/__init__.py
@@ -1,2 +1,2 @@
 """Package defines stain norm utils for the toolbox"""
-from tiatoolbox.tools import stain_extractor, abc_stain_extractor
+from tiatoolbox.tools.stain_utils import stain_extractor, abc_stain_extractor
