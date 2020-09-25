e09b8bc3b4a03e39bb27d639c48ea6586497a723
John Pocock <j.c.pocock@warwick.ac.uk>
2020-09-25 14:23:42 +0100

    TST: Simplify Test Slide Path Logic

--- tests/test_tiatoolbox.py
+++ tests/test_tiatoolbox.py
@@ -485,22 +485,14 @@ def test_wsimeta_validate_init_pass():
 
 
 def test_wsimeta_openslidewsireader_ndpi(_response_ndpi, tmp_path):
-    file_types = ("*.ndpi",)
-    files_all = utils.misc.grab_files_from_dir(
-        input_path=str(pathlib.Path(__file__).parent), file_types=file_types,
-    )
-    input_dir, file_name, ext = utils.misc.split_path_name_ext(str(files_all[0]))
+    input_dir, file_name, ext = utils.misc.split_path_name_ext(str(_response_ndpi))
     wsi_obj = wsireader.OpenSlideWSIReader(input_dir, file_name + ext)
     meta = wsi_obj.slide_info
     assert meta.validate()
 
 
 def test_wsimeta_openslidewsireader_svs(_response_svs, tmp_path):
-    file_types = ("*.svs",)
-    files_all = utils.misc.grab_files_from_dir(
-        input_path=str(pathlib.Path(__file__).parent), file_types=file_types,
-    )
-    input_dir, file_name, ext = utils.misc.split_path_name_ext(str(files_all[0]))
+    input_dir, file_name, ext = utils.misc.split_path_name_ext(str(_response_svs))
     wsi_obj = wsireader.OpenSlideWSIReader(input_dir, file_name + ext)
     meta = wsi_obj.slide_info
     assert meta.validate()
