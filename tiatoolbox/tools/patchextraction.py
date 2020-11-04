ae3af51d7c19de520f59fd024a909e6428be6d98
shaneahmed <sa>
2020-11-04 13:32:35 +0000

    DOC: Add Licence block - Add licence block

--- tiatoolbox/tools/patchextraction.py
+++ tiatoolbox/tools/patchextraction.py
@@ -1,3 +1,27 @@
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# This file contains code inspired by StainTools
+# [https://github.com/Peter554/StainTools] written by Peter Byfield.
+#
+# The Original Code is Copyright (C) 2020, TIALab, University of Warwick
+# All rights reserved.
+# ***** END GPL LICENSE BLOCK *****
+
+"""This file defines patch extraction methods for deep learning models."""
 from abc import ABC
 from tiatoolbox.utils.exceptions import MethodNotSupported
 from tiatoolbox.utils.misc import imread
@@ -143,13 +167,20 @@ class PointsPatchExtractor(PatchExtractor):
         img_patch_w: input image patch width
     """
 
-    def __init__(self, img_patch_h, img_patch_w, pad_y, pad_x, input_points, num_examples_per_patch=9  # Square Root of Num of Examples must be odd
+    def __init__(
+        self,
+        img_patch_h,
+        img_patch_w,
+        pad_y,
+        pad_x,
+        input_points,
+        num_examples_per_patch=9,  # Square Root of Num of Examples must be odd
     ):
         super(PatchExtractor, self).__init__(
             img_patch_h=img_patch_h, img_patch_w=img_patch_w, pad_y=pad_y, pad_x=pad_x
         )
         self.input_points = input_points
-        self.num_examples_per_patch=num_examples_per_patch
+        self.num_examples_per_patch = num_examples_per_patch
 
     def extract_patches(
         self, input_img, labels=None, save_output=False, save_path=None, save_name=None
@@ -159,7 +190,7 @@ class PointsPatchExtractor(PatchExtractor):
         else:
             raise Exception("Please input correct csv, json path or csv data")
 
-        if type(input_img) == str:
+        if input_img == str:
             image = imread(input_img)
         elif isinstance(input_img, np.ndarray):
             image = input_img
@@ -180,19 +211,15 @@ class PointsPatchExtractor(PatchExtractor):
             (num_patches_img, patch_h, patch_w, image.shape[2]), dtype=image.dtype
         )
         labels = []
-        cell_id = []
+        class_id = []
         for i in range(num_patches_img):
             labels.append([])
-            cell_id.append([])
-
-        img_h = np.size(image, 0)
-        img_w = np.size(image, 1)
-        img_d = np.size(image, 2)
+            class_id.append([])
 
         cell_tot = 1
         iter_tot = 0
         for row in input_points:
-            cell_type = row[0]
+            patch_label = row[0]
             cell_location = np.array([row[2], row[1]], dtype=np.int)
             cell_location[0] = (
                 cell_location[0] + self.pad_y - 1
@@ -214,13 +241,13 @@ class PointsPatchExtractor(PatchExtractor):
                     start_w = cell_location[1] - w - int((patch_w - 1) / 2)
                     end_h = start_h + patch_h
                     end_w = start_w + patch_w
-                    labels[iter_tot] = cell_type
-                    cell_id[iter_tot] = cell_tot
+                    labels[iter_tot] = patch_label
+                    class_id[iter_tot] = cell_tot
                     img_patches[iter_tot, :, :, :] = image[start_h:end_h, start_w:end_w]
                     iter_tot += 1
 
             cell_tot += 1
-        return img_patches, labels, cell_id
+        return img_patches, labels, class_id
 
     def merge_patches(self, patches=None):
         raise MethodNotSupported(
