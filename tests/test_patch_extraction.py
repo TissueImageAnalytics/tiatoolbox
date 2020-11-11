b7ccc95b3359818f5718230f4263a2e42626f9f4
shaneahmed <sa>
2020-11-11 13:02:51 +0000

    TST: Update tests for restructured PatchExtraction - Update tests for restructured PatchExtraction.

--- tests/test_patch_extraction.py
+++ tests/test_patch_extraction.py
@@ -1,13 +1,22 @@
 from tiatoolbox.tools import patchextraction
 from tiatoolbox.utils.exceptions import MethodNotSupported
+from tiatoolbox.utils.misc import imread
 
 import pytest
+import pathlib
 
 
 def test_get_patch_extractor():
     """Test get_patch_extractor returns the right object."""
+    file_parent_dir = pathlib.Path(__file__).parent
+    input_image = imread(file_parent_dir.joinpath("data/source_image.png"))
+    labels = file_parent_dir.joinpath("data/sample_patch_extraction.csv")
     points = patchextraction.get_patch_extractor(
-        "point", img_patch_h=200, img_patch_w=200
+        input_image=input_image,
+        labels=labels,
+        method_name="point",
+        img_patch_h=200,
+        img_patch_w=200,
     )
 
     assert isinstance(points, patchextraction.PointsPatchExtractor)
@@ -16,13 +25,19 @@ def test_get_patch_extractor():
         points.merge_patches()
 
     fixed_window = patchextraction.get_patch_extractor(
-        "fixedwindow", img_patch_h=200, img_patch_w=200
+        input_image=input_image,
+        method_name="fixedwindow",
+        img_patch_h=200,
+        img_patch_w=200,
     )
 
     assert isinstance(fixed_window, patchextraction.FixedWindowPatchExtractor)
 
     variable_window = patchextraction.get_patch_extractor(
-        "variablewindow", img_patch_h=200, img_patch_w=200
+        input_image=input_image,
+        method_name="variablewindow",
+        img_patch_h=200,
+        img_patch_w=200,
     )
 
     assert isinstance(variable_window, patchextraction.VariableWindowPatchExtractor)
@@ -32,8 +47,11 @@ def test_get_patch_extractor():
 
 
 def test_points_patch_extractor():
+    file_parent_dir = pathlib.Path(__file__).parent
+    input_image = imread(file_parent_dir.joinpath("data/source_image.png"))
+    labels = file_parent_dir.joinpath("data/sample_patch_extraction.csv")
     points = patchextraction.get_patch_extractor(
-        "point", img_patch_h=200, img_patch_w=200
+        input_image=input_image, labels=labels, method_name="point", img_patch_h=200, img_patch_w=200
     )
 
     with pytest.raises(MethodNotSupported):
