f89dedef6d7b21dbd2a7a655ce9c8b0934b87bc6
shaneahmed <sa>
2020-11-11 13:00:24 +0000

    API: Restructure PatchExtraction - Restructure PatchExtraction as an iterator.

--- tiatoolbox/tools/patchextraction.py
+++ tiatoolbox/tools/patchextraction.py
@@ -21,10 +21,10 @@
 """This file defines patch extraction methods for deep learning models."""
 from abc import ABC
 from tiatoolbox.utils.exceptions import MethodNotSupported
-from tiatoolbox.utils.misc import imread
+# from tiatoolbox.utils.misc import imread
 
 # import math
-import numpy as np
+# import numpy as np
 
 
 class PatchExtractor(ABC):
@@ -32,18 +32,31 @@ class PatchExtractor(ABC):
     Class for extracting and merging patches in standard and whole-slide images.
 
     Args:
+        input_image(str, pathlib.Path, ndarray): input image for patch extraction.
         img_patch_h(int): input image patch height.
         img_patch_w(int): input image patch width.
         pad_y(int): symmetric padding y-axis.
         pad_x(int): symmetric padding x-axis.
 
+    Attributes:
+        input_image(ndarray, WSIReader): input image for patch extraction.
+          input_image type is ndarray for an image tile whereas :obj:`WSIReader`
+          for an WSI.
+        img_patch_h(int): input image patch height.
+        img_patch_w(int): input image patch width.
+        pad_y(int): symmetric padding y-axis.
+        pad_x(int): symmetric padding x-axis.
+        n (int): current state of the iterator.
+
     """
 
-    def __init__(self, img_patch_h, img_patch_w, pad_y=0, pad_x=0):
+    def __init__(self, input_image, img_patch_h, img_patch_w, pad_y=0, pad_x=0):
         self.img_patch_h = img_patch_h
         self.img_patch_w = img_patch_w
         self.pad_y = pad_y
         self.pad_x = pad_x
+        self.n = 0
+        self.input_image = input_image
 
     def __iter__(self):
         self.n = 0
@@ -68,19 +81,16 @@ class PatchExtractor(ABC):
     #     last_step = (nr_step + 1) * stride
     #     return int(last_step)
 
-    def extract_patches(
-        self, input_img, labels=None
-    ):
-        """Extract patches from an image using locations provided by labels data.
-
-        Args:
-            input_img (str, ndarray): input image.
-            labels (str, ndarray):
-
-        Returns:
-            img_patches (ndarray): extracted image patches of size NxHxWxD.
-        """
-        raise NotImplementedError
+    # def extract_patches(self):
+    #     """Extract patches from an image using locations provided by labels data.
+    #
+    #     Args:
+    #         labels (str, ndarray):
+    #
+    #     Returns:
+    #         img_patches (ndarray): extracted image patches of size NxHxWxD.
+    #     """
+    #     raise NotImplementedError
 
     def merge_patches(self, patches):
         """Merge the patch-level results to get the overall image-level prediction.
@@ -98,25 +108,32 @@ class FixedWindowPatchExtractor(PatchExtractor):
     """Extract and merge patches using fixed sized windows for images and labels.
 
     Args:
-        img_patch_h: input image patch height
-        img_patch_w: input image patch width
-        stride_h: stride in horizontal direction for patch extraction
-        stride_w: stride in vertical direction for patch extraction
+        stride_h(int): stride in horizontal direction for patch extraction.
+        stride_w(int): stride in vertical direction for patch extraction.
     """
 
     def __init__(
-        self, img_patch_h, img_patch_w, pad_y=0, pad_x=0, stride_h=1, stride_w=1
+        self,
+        input_image,
+        img_patch_h,
+        img_patch_w,
+        pad_y=0,
+        pad_x=0,
+        stride_h=1,
+        stride_w=1,
     ):
         super().__init__(
-            img_patch_h=img_patch_h, img_patch_w=img_patch_w, pad_y=pad_y, pad_x=pad_x
+            input_image=input_image,
+            img_patch_h=img_patch_h,
+            img_patch_w=img_patch_w,
+            pad_y=pad_y,
+            pad_x=pad_x,
         )
         self.stride_h = stride_h
         self.stride_w = stride_w
 
-    def extract_patches(
-        self, input_img, labels=None
-    ):
-        raise NotImplementedError
+    # def extract_patches(self, input_img, labels=None):
+    #     raise NotImplementedError
 
     def merge_patches(self, patches):
         raise NotImplementedError
@@ -126,8 +143,6 @@ class VariableWindowPatchExtractor(PatchExtractor):
     """Extract and merge patches using variable sized windows for images and labels.
 
     Args:
-        img_patch_h: input image patch height
-        img_patch_w: input image patch width
         stride_h: stride in horizontal direction for patch extraction
         stride_w: stride in vertical direction for patch extraction
         label_patch_h: network output label height
@@ -136,6 +151,7 @@ class VariableWindowPatchExtractor(PatchExtractor):
 
     def __init__(
         self,
+        input_image,
         img_patch_h,
         img_patch_w,
         pad_y=0,
@@ -146,17 +162,19 @@ class VariableWindowPatchExtractor(PatchExtractor):
         label_patch_w=None,
     ):
         super().__init__(
-            img_patch_h=img_patch_h, img_patch_w=img_patch_w, pad_y=pad_y, pad_x=pad_x
+            input_image=input_image,
+            img_patch_h=img_patch_h,
+            img_patch_w=img_patch_w,
+            pad_y=pad_y,
+            pad_x=pad_x,
         )
         self.stride_h = stride_h
         self.stride_w = stride_w
         self.label_patch_h = label_patch_h
         self.label_patch_w = label_patch_w
 
-    def extract_patches(
-        self, input_img, labels=None
-    ):
-        raise NotImplementedError
+    # def extract_patches(self, input_img, labels=None):
+    #     raise NotImplementedError
 
     def merge_patches(self, patches):
         raise NotImplementedError
@@ -166,88 +184,99 @@ class PointsPatchExtractor(PatchExtractor):
     """Extracting patches with specified points as a centre.
 
     Args:
-        img_patch_h (int): input image patch height
-        img_patch_w (int): input image patch width
-        pad_y (int): symmetric padding y-axis.
-        pad_x (int): symmetric padding x-axis.
+        labels(ndarray, pd.DataFrame, str, pathlib.Path): contains location and/or
+         type of patch. Input can be path to a csv or json file.
+        num_examples_per_patch(int): Number of examples per patch for ensemble
+         classification, default=9 (centre of patch and all the eight neighbours as
+         centre).
+
+    Attributes:
+        labels(pd.DataFrame): A table containing location and/or type of patch.
+        num_examples_per_patch(int): Number of examples per patch for ensemble
+         classification.
 
     """
 
     def __init__(
         self,
-        img_patch_h,
-        img_patch_w,
+        input_image,
+        labels,
+        img_patch_h=224,
+        img_patch_w=224,
         pad_y=0,
         pad_x=0,
-        num_examples_per_patch=9,  # Square Root of Num of Examples must be odd
+        num_examples_per_patch=9,
     ):
         super().__init__(
-            img_patch_h=img_patch_h, img_patch_w=img_patch_w, pad_y=pad_y, pad_x=pad_x
+            input_image=input_image,
+            img_patch_h=img_patch_h,
+            img_patch_w=img_patch_w,
+            pad_y=pad_y,
+            pad_x=pad_x,
         )
 
         self.num_examples_per_patch = num_examples_per_patch
+        self.labels = labels
 
-    def extract_patches(
-        self, input_img, labels=None
-    ):
-        if not isinstance(labels, np.ndarray):
-            raise Exception("Please input correct csv, json path or csv data")
-
-        if input_img == str:
-            image = imread(input_img)
-        elif isinstance(input_img, np.ndarray):
-            image = input_img
-        else:
-            raise Exception("Please input correct image path or numpy array")
-
-        patch_h = self.img_patch_h
-        patch_w = self.img_patch_w
-
-        image = np.lib.pad(
-            image,
-            ((self.pad_y, self.pad_y), (self.pad_x, self.pad_x), (0, 0)),
-            "symmetric",
-        )
-
-        num_patches_img = len(labels) * self.num_examples_per_patch
-        img_patches = np.zeros(
-            (num_patches_img, patch_h, patch_w, image.shape[2]), dtype=image.dtype
-        )
-        labels = [None] * num_patches_img
-        class_id = [None] * num_patches_img
-
-        cell_tot = 1
-        iter_tot = 0
-        for row in labels:
-            patch_label = row[0]
-            cell_location = np.array([row[2], row[1]], dtype=np.int)
-            cell_location[0] = (
-                cell_location[0] + self.pad_y - 1
-            )  # Python index starts from 0
-            cell_location[1] = (
-                cell_location[1] + self.pad_x - 1
-            )  # Python index starts from 0
-            if self.num_examples_per_patch > 1:
-                root_num_examples = np.sqrt(self.num_examples_per_patch)
-                start_location = -int(root_num_examples / 2)
-                end_location = int(root_num_examples + start_location)
-            else:
-                start_location = 0
-                end_location = 1
-
-            for h in range(start_location, end_location):
-                for w in range(start_location, end_location):
-                    start_h = cell_location[0] - h - int((patch_h - 1) / 2)
-                    start_w = cell_location[1] - w - int((patch_w - 1) / 2)
-                    end_h = start_h + patch_h
-                    end_w = start_w + patch_w
-                    labels[iter_tot] = patch_label
-                    class_id[iter_tot] = cell_tot
-                    img_patches[iter_tot, :, :, :] = image[start_h:end_h, start_w:end_w]
-                    iter_tot += 1
-
-            cell_tot += 1
-        return img_patches, labels, class_id
+    # def extract_patches(self, input_img, labels=None):
+    #     if not isinstance(labels, np.ndarray):
+    #         raise Exception("Please input correct csv, json path or csv data")
+    #
+    #     if input_img == str:
+    #         image = imread(input_img)
+    #     elif isinstance(input_img, np.ndarray):
+    #         image = input_img
+    #     else:
+    #         raise Exception("Please input correct image path or numpy array")
+    #
+    #     patch_h = self.img_patch_h
+    #     patch_w = self.img_patch_w
+    #
+    #     image = np.lib.pad(
+    #         image,
+    #         ((self.pad_y, self.pad_y), (self.pad_x, self.pad_x), (0, 0)),
+    #         "symmetric",
+    #     )
+    #
+    #     num_patches_img = len(labels) * self.num_examples_per_patch
+    #     img_patches = np.zeros(
+    #         (num_patches_img, patch_h, patch_w, image.shape[2]), dtype=image.dtype
+    #     )
+    #     labels = [None] * num_patches_img
+    #     class_id = [None] * num_patches_img
+    #
+    #     cell_tot = 1
+    #     iter_tot = 0
+    #     for row in labels:
+    #         patch_label = row[0]
+    #         cell_location = np.array([row[2], row[1]], dtype=np.int)
+    #         cell_location[0] = (
+    #             cell_location[0] + self.pad_y - 1
+    #         )  # Python index starts from 0
+    #         cell_location[1] = (
+    #             cell_location[1] + self.pad_x - 1
+    #         )  # Python index starts from 0
+    #         if self.num_examples_per_patch > 1:
+    #             root_num_examples = np.sqrt(self.num_examples_per_patch)
+    #             start_location = -int(root_num_examples / 2)
+    #             end_location = int(root_num_examples + start_location)
+    #         else:
+    #             start_location = 0
+    #             end_location = 1
+    #
+    #         for h in range(start_location, end_location):
+    #             for w in range(start_location, end_location):
+    #                 start_h = cell_location[0] - h - int((patch_h - 1) / 2)
+    #                 start_w = cell_location[1] - w - int((patch_w - 1) / 2)
+    #                 end_h = start_h + patch_h
+    #                 end_w = start_w + patch_w
+    #                 labels[iter_tot] = patch_label
+    #                 class_id[iter_tot] = cell_tot
+    #              img_patches[iter_tot, :, :, :] = image[start_h:end_h, start_w:end_w]
+    #                 iter_tot += 1
+    #
+    #         cell_tot += 1
+    #     return img_patches, labels, class_id
 
     def merge_patches(self, patches=None):
         """Merge patch is not supported by :obj:`PointsPatchExtractor`.
