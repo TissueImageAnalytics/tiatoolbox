f77f3105ef1647af413a56ab6eefc1bfa7e14a27
mostafajahanifar <mostafa.jahanifar@gmail.com>
2020-12-18 09:25:02 +0000

    MAINT: Clean up residual codes

--- tiatoolbox/utils/misc.py
+++ tiatoolbox/utils/misc.py
@@ -239,56 +239,6 @@ def mpp2objective_power(mpp):
     raise ValueError()
 
 
-def image_luminosity_standardiser(img, percentile=0.95):
-    """Standardize (adjust) the luminosity component of the input RGB image.
-
-    Args:
-        img (ndarray): input image used to obtain tissue mask.
-            Image should be RGB uint8.
-        percentile (int): Percentile for luminosity saturation.
-            At least (100 - percentile)% of pixels should be
-            fully luminous (white).
-
-    Returns:
-        img (ndarray): Image uint8 RGB with standardized brightness.
-
-    Examples:
-        >>> from tiatoolbox import utils
-        >>> img = utils.misc.image_luminosity_standardiser(img, percentile=0.95)
-
-    """
-    assert img.dtype == np.uint8, "Image should be RGB uint8."
-    img_LAB = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-    L_float = img_LAB[:, :, 0].astype(float)
-    p = np.percentile(L_float, percentile)
-    img_LAB[:, :, 0] = np.clip(255 * L_float / p, 0, 255).astype(np.uint8)
-    img = cv2.cvtColor(img_LAB, cv2.COLOR_LAB2RGB)
-    return img
-
-
-def luminosity_standardiser(lum, percentile=0.95):
-    """Standardize (adjust) the luminosity component.
-
-    Args:
-        lum (ndarray): input Luminosity component of an image used to
-            obtain tissue mask. lum must be in range of 0-255.
-        percentile (int): Percentile for luminosity saturation. At least
-        (100 - percentile)% of pixels should be fully luminous (white).
-
-    Returns:
-        lum (ndarray): Luminosity component with standardized brightness.
-
-    Examples:
-        >>> from tiatoolbox import utils
-        >>> lum_out = utils.misc.luminosity_standardiser(lum_in, percentile=0.95)
-
-    """
-    l_float = lum.astype(float)
-    perc = np.percentile(l_float, percentile)
-    lum = np.clip(255 * l_float / perc, 0, 255).astype(np.uint8)
-    return lum
-
-
 def contrast_enhancer(img, low_p=2, high_p=98):
     """Enhancing contrast of the input RGB image using intensity adjustment through
         image low and high percentiles.
@@ -317,25 +267,3 @@ def contrast_enhancer(img, low_p=2, high_p=98):
         img_out = exposure.rescale_intensity(img_out, in_range=(p_low, p_high),
                                              out_range=(0., 255.))
     return np.uint8(img_out)
-
-
-# just for temporary testing
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    img = imread('D:/PythonProjects/tiatoolbox/tests/data/source_image.png')
-
-    # showing original RGB image and its luminosity standardized counterpart
-    plt.figure('Image'), plt.imshow(img)
-    img_luminosity_std = image_luminosity_standardiser(img, percentile=0.95)
-    plt.figure('img_luminosity_std'), plt.imshow(img_luminosity_std)
-    img_LAB = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-    plt.figure('luminosity'), plt.imshow(img_LAB[:, :, 0]/255.)
-    img_luminosity_std_LAB = cv2.cvtColor(img_luminosity_std, cv2.COLOR_RGB2LAB)
-    plt.figure('luminosity_std'), plt.imshow(img_luminosity_std_LAB[:, :, 0]/255.)
-
-    # using image contract enhancement
-    img_contrast = contrast_enhancer(img, low_p=2, high_p=98)
-    plt.figure('Image Contrast enhanced'), plt.imshow(img_contrast)
-    img_contrast_LAB = cv2.cvtColor(img_contrast, cv2.COLOR_RGB2LAB)
-    plt.figure('luminosity_contrasted'), plt.imshow(img_contrast_LAB[:, :, 0]/255.)
-    plt.show()
