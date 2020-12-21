cffcd7ca8d367821189c979d36736f61581cee47
John Pocock <j.c.pocock@warwick.ac.uk>
2020-12-21 17:36:38 +0000

    MAINT: Refactor Tissue Maskers

--- tiatoolbox/tools/tissuemask.py
+++ tiatoolbox/tools/tissuemask.py
@@ -24,7 +24,6 @@
 """Methods of masking tissue and background."""
 
 from abc import ABC, abstractmethod
-import warnings
 
 import cv2
 import numpy as np
@@ -33,16 +32,21 @@ from tiatoolbox.utils.misc import objective_power2mpp
 
 
 class TissueMasker(ABC):
-    def fit(self, image=None, **kwargs):
+    def __init__(self) -> None:
+        super().__init__()
+        self.fitted = False
+
+    def fit(self, image=None, mask=None) -> None:
         """Fit the masker to the image and given key word parameters.
 
         Args:
             image (np.ndarray): RGB image, usually a WSI thumbnail.
-            kwargs (dict): Generic key word arguments.
+            mask (np.ndarray): Target/ground-truth mask.
         """
+        self.fitted = True
 
     @abstractmethod
-    def transform(self, image: np.ndarray):
+    def transform(self, image: np.ndarray) -> np.ndarray:
         """Create and return a tissue mask.
 
         Args:
@@ -52,31 +56,55 @@ class TissueMasker(ABC):
             np.ndarary: Map of semantic classes spatially over the WSI
                 e.g. regions of tissue vs background.
         """
-        return
+        if not self.fitted:
+            raise Exception("Fit must be called before transform.")
 
-    def fit_transform(self, image: np.ndarray, **kwargs):
-        """Apply fit and transform in one step.
+    def fit_transform(self, image: np.ndarray, **fit_params) -> np.ndarray:
+        """Apply :func:`fit` and :func:`transform` in one step.
+
+        Sometimes it can be more optimal to perform both at the same
+        time for a single sample. In this case the base implementation
+        of :fun:`fit` followed by :func:`transform` can be overridden.
 
         Args:
             image (np.ndarray): Image to create mask from.
-            kwargs (dict): Generic key word arguments passed to fit.
+            fit_params (dict): Generic key word arguments passed to fit.
         """
-        self.fit(image, **kwargs)
+        self.fit(image, **fit_params)
         return self.transform(image)
 
 
 class OtsuTissueMasker(TissueMasker):
     """Tissue masker which uses Otsu's method to determine background."""
 
-    def transform(self, image: np.ndarray) -> np.ndarray:
+    def __init__(self) -> None:
+        super().__init__()
+        self.threshold = None
+
+    def fit(self, image: np.ndarray, mask=None) -> None:
+        # find Otsu's threshold
         if len(image.shape) == 3:
             gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         else:
             gray = image
 
-        mask, _ = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        self.threshold, _ = cv2.threshold(
+            gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        )
 
-        return mask
+        self.fitted = True
+
+    def transform(self, image: np.ndarray) -> np.ndarray:
+        super().transform(image)
+
+        if len(image.shape) == 3:
+            gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        else:
+            gray = image
+
+        mask = gray < self.threshold
+
+        return mask.astype(np.bool)
 
 
 class MorphologicalMasker(TissueMasker):
@@ -86,82 +114,96 @@ class MorphologicalMasker(TissueMasker):
     removal, followed by a morphological dilation. The kernel for the
     dilation is an ellipse of radius 64/mpp unless a value is given for
     kernel_size to :func:`fit`. MPP is estimated from objective power
-    via func:`tiatoolbox.utils.misc.objective_power2mpp` if unknown. For
-    small region removal, the area of the kernel is used as a threshold.
+    via func:`tiatoolbox.utils.misc.objective_power2mpp` if a power
+    argument is given instead of mpp to the initialiser.
+    For small region removal, the minimum area size defaults to the area
+    of the kernel.
     """
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.kernel_size = np.array([1, 1])
-        self.min_region_size = 3
-        self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, self.kernel_size)
-
-    def fit(self, image: np.ndarray = None, **kwargs) -> np.ndarray:
-        """Fit the masker to the image and given key word parameters.
-
-        At least one of mpp, power, or kernel_size is required.
+    def __init__(
+        self, *, mpp=None, power=None, kernel_size=None, min_region_size=None
+    ) -> None:
+        """Initialise a morphological masker.
 
         Args:
-            thumbnail (np.ndarray): An RGB thumnail image.
-            kwargs (dict): Key word arguements.
-            kernel_size (int or tuple of int): Manual kernel size, optional.
-            mpp (float or tuple of float): The microns per-pixel of
-                the image. Used to calculate kernel_size, optional.
-            power (float or tuple of float): The objective
-                power of the image. Used to calculate kernel_size, optional.
+            mpp (float or tuple of float):
+                The microns per-pixel of the image to be masked. Used to
+                calculate kernel_size a 64/mpp, optional.
+            power (float or tuple of float):
+                The objective power of the image to be masked. Used to
+                calculate kernel_size as 64/objective_power2mpp(power),
+                optional.
+            kernel_size (int or tuple of int):
+                Size of elliptical kernel in x and y, optional.
+            min_region_size (int):
+                Minimum region size in pixels to consider as foreground.
+                Defaults to area of the kernel.
         """
-        mpp = kwargs.get("mpp")
-        power = kwargs.get("power")
-        kernel_size = kwargs.get("kernel_size")
+        super().__init__()
 
-        # Check kwargs
-        if all(arg is None for arg in [mpp, power, kernel_size]):
-            warnings.warn(
-                "No mpp, power, or kernel_size given. "
-                "Using default kernel size of 1. "
-                "Masking may be inconsistent across slides of different resolutions."
-            )
-            kernel_size = self.kernel_size
+        self.min_region_size = min_region_size
+        self.threshold = None
 
+        # Check for conflicting arguments
         if sum(arg is not None for arg in [mpp, power, kernel_size]) > 1:
             raise ValueError("Only one of mpp, power, kernel_size can be given.")
 
-        # Convert (objective) power to MPP
+        # Default to kernel_size of (1, 1) if no arguments given
+        if all(arg is None for arg in [mpp, power, kernel_size]):
+            kernel_size = np.array([1, 1])
+
+        # Convert (objective) power approximately to MPP to unify units
         if power is not None:
             mpp = objective_power2mpp(power)
 
-        # Normalise args to be either None or a length 2 numpy array
+        # Convert MPP to an integer kernel_size
         if mpp is not None:
             mpp = np.array(mpp)
             if mpp.size != 2:
                 mpp = mpp.repeat(2)
             kernel_size = np.max([64 / mpp, [1, 1]], axis=0)
-        if kernel_size is not None:
-            kernel_size = np.array(kernel_size)
-            if kernel_size.size != 2:
-                kernel_size = kernel_size.repeat(2)
 
-        self.kernel_size = kernel_size
+        # Ensure kernel_size is a length 2 numpy array
+        kernel_size = np.array(kernel_size)
+        if kernel_size.size != 2:
+            kernel_size = kernel_size.repeat(2)
 
-        # Scale the kernel with MPP for consistent results across resolutions
-        if mpp is not None:
-            self.kernel_size = np.max([64 / mpp, [1, 1]], axis=0)
-
-        # At this point _kernel_size should not be None
-        if self.kernel_size is None or None in self.kernel_size:
+        # Sanity check: At this point _kernel_size should not be None
+        if kernel_size is None or None in kernel_size:
             raise AssertionError("Internal _kernel_size is None")
 
-        self.kernel_size = tuple(np.round(self.kernel_size).astype(int))
-        self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, self.kernel_size)
-        self.min_region_size = np.sum(self.kernel)
+        # Convert to an integer double/ pair
+        self.kernel_size = tuple(np.round(kernel_size).astype(int))
 
-    def transform(self, image: np.ndarray):
+        # Create structuring element for morphological operations
+        self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, self.kernel_size)
+
+        # Set min region size to kernel area if None
+        if self.min_region_size is None:
+            self.min_region_size = np.sum(self.kernel)
+
+    def fit(self, image: np.ndarray = None, mask=None) -> np.ndarray:
+        # Find Otsu's threshold
         if len(image.shape) == 3:
             gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         else:
             gray = image
 
-        _, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        self.threshold, _ = cv2.threshold(
+            gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+        )
+
+        self.fitted = True
+
+    def transform(self, image: np.ndarray):
+        super().transform(image)
+
+        if len(image.shape) == 3:
+            gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        else:
+            gray = image
+
+        mask = (gray < self.threshold).astype(np.uint8)
 
         _, output, stats, _ = cv2.connectedComponentsWithStats(mask, connectivity=8)
         sizes = stats[1:, -1]
@@ -171,4 +213,4 @@ class MorphologicalMasker(TissueMasker):
 
         mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, self.kernel)
 
-        return mask
+        return mask.astype(np.bool)
