28011788ad6c62088831e86b2e0a8d9384fe325b
simongraham <s.graham.1@warwick.ac.uk>
2021-04-29 16:07:59 +0100

    MAINT: Added dummy test for protyping

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,26 +1,12 @@
-import random
-import os
-import glob
-import cv2
-import os
-
-import torch
-import torch.utils.data as torch_data
-import torchvision.transforms as transforms
-
-import sys
-
-sys.path.append("..")
-
-
 from tiatoolbox.models.classification import CNN_Patch_Predictor
+from tiatoolbox.models.dataset import Patch_Dataset
+from tiatoolbox.utils.exceptions import MethodNotSupported, FileNotSupported
+from tiatoolbox.utils.misc import imread
+
+import pytest
+import pathlib
+import numpy as np
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-random.seed(5)
-model = CNN_Patch_Predictor(
-    backbone="resnet18", pretrained="kather", batch_size=16, nr_loader_worker=4
-)
-output = model.predict("Kather_Sample/all/", return_names=True)
-print(output)
+def test():
+    return
