803f52b7e0fbc623ca52818130def962b345182f
John Pocock <j.c.pocock@warwick.ac.uk>
2021-04-30 22:02:02 +0100

    TST: Improve Test Coverage

--- tests/test_tissuemask.py
+++ tests/test_tissuemask.py
@@ -104,16 +104,21 @@ def test_morphological_masker_mpp(_sample_svs):
     wsi = wsireader.OpenSlideWSIReader(_sample_svs)
     mpp = 32
     thumb = wsi.slide_thumbnail(mpp, "mpp")
-    masker = tissuemask.MorphologicalMasker(mpp=mpp)
-    mask_a = masker.fit_transform([thumb])[0]
+    kwarg_sets = [
+        dict(mpp=mpp),
+        dict(mpp=[mpp, mpp]),
+    ]
+    for kwargs in kwarg_sets:
+        masker = tissuemask.MorphologicalMasker(**kwargs)
+        mask_a = masker.fit_transform([thumb])[0]
 
-    masker.fit([thumb])
-    mask_b = masker.transform([thumb])[0]
+        masker.fit([thumb])
+        mask_b = masker.transform([thumb])[0]
 
-    assert np.array_equal(mask_a, mask_b)
-    assert masker.threshold is not None
-    assert len(np.unique(mask_a)) == 2
-    assert mask_a.shape == thumb.shape[:2]
+        assert np.array_equal(mask_a, mask_b)
+        assert masker.threshold is not None
+        assert len(np.unique(mask_a)) == 2
+        assert mask_a.shape == thumb.shape[:2]
 
 
 def test_morphological_masker_power(_sample_svs):
