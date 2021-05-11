fb95a345c19a2eed099a9e86fd2fc664121c4f8c
shaneahmed <sa>
2021-05-11 13:29:09 +0100

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tialab/tiatoolbox",
-    version="0.5.2",
+    version="0.6.0",
     zip_safe=False,
 )
