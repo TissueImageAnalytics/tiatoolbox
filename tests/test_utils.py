81df7a27dbd80f32b3e263b9e5abf16939e682ef
vqdang <dangvuquoc1993@gmail.com>
2021-05-15 11:46:33 +0100

    UPD: hash check download

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,7 @@ import random
 import shutil
 from pathlib import Path
 from typing import Tuple
+import hashlib
 
 import cv2
 import numpy as np
@@ -769,7 +770,26 @@ def test_download_data():
     save_zip_path = os.path.join(save_dir_path, "test_directory.zip")
 
     misc.download_data(url, save_zip_path, overwrite=True)  # overwrite
-    misc.download_data(url, save_zip_path, overwrite=True)  # data already exists
+    old_hash = hashlib.md5(open(save_zip_path, 'rb').read()).hexdigest()
+    # modify the content
+    with open(save_zip_path, 'wb') as fptr:
+        fptr.write('dataXXX'.encode())  # random data
+    bad_hash = hashlib.md5(open(save_zip_path, 'rb').read()).hexdigest()
+    assert old_hash != bad_hash
+    misc.download_data(url, save_zip_path, overwrite=True)  # overwrite
+    new_hash = hashlib.md5(open(save_zip_path, 'rb').read()).hexdigest()
+    assert new_hash == old_hash
+
+    # test not overiting
+    # modify the content
+    with open(save_zip_path, 'wb') as fptr:
+        fptr.write('dataXXX'.encode())  # random data
+    bad_hash = hashlib.md5(open(save_zip_path, 'rb').read()).hexdigest()
+    assert old_hash != bad_hash
+    misc.download_data(url, save_zip_path, overwrite=False)  # data already exists
+    new_hash = hashlib.md5(open(save_zip_path, 'rb').read()).hexdigest()
+    assert new_hash == bad_hash
+
     shutil.rmtree(save_dir_path, ignore_errors=True)  # remove data
     misc.download_data(url, save_zip_path)  # to test skip download
     assert os.path.exists(save_zip_path)
