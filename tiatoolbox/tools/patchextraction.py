90860f083bb2238999b54455271c1b6e4f3f21bf
Mostafa Jahanifar <74412979+mostafajahanifar@users.noreply.github.com>
2021-06-01 11:56:01 +0100

    API: changing `FixedWindowPatchExtractor` to `SlidingWindowPatchExtractor` (#121)
    - Change the class `FixedWindowPatchExtractor` name to  `SlidingWindowPatchExtractor` 
    - get_patchextractor takes the `slidingwindow` as an argument. 
    - Depreciate `VariableWindowPatchExtractor`
    
    Co-authored-by: @mostafajahanifar 
    Co-authored-by: @shaneahmed

--- tiatoolbox/tools/patchextraction.py
+++ tiatoolbox/tools/patchextraction.py
@@ -161,21 +161,9 @@ class PatchExtractor(ABC):
 
         return self
 
-    def merge_patches(self, patches):
-        """Merge the patch-level results to get the overall image-level prediction.
 
-        Args:
-            patches: patch-level predictions
-
-        Returns:
-            image: merged prediction
-
-        """
-        raise NotImplementedError
-
-
-class FixedWindowPatchExtractor(PatchExtractor):
-    """Extract and merge patches using fixed sized windows for images and labels.
+class SlidingWindowPatchExtractor(PatchExtractor):
+    """Extract patches using sliding fixed sized window for images and labels.
 
     Args:
         stride(int or tuple(int)): stride in (x, y) direction for patch extraction,
@@ -213,51 +201,6 @@ class FixedWindowPatchExtractor(PatchExtractor):
                 self.stride = (int(stride), int(stride))
 
         self._generate_location_df()
-
-    def merge_patches(self, patches):
-        raise NotImplementedError
-
-
-class VariableWindowPatchExtractor(PatchExtractor):
-    """Extract and merge patches using variable sized windows for images and labels.
-
-    Args:
-        stride(tuple(int)): stride in (x, y) direction for patch extraction.
-        label_patch_size(tuple(int)): network output label (width, height).
-
-    Attributes:
-        stride(tuple(int)): stride in (x, y) direction for patch extraction.
-        label_patch_size(tuple(int)): network output label (width, height).
-
-    """
-
-    def __init__(
-        self,
-        input_img,
-        patch_size,
-        resolution=0,
-        units="level",
-        stride=(1, 1),
-        pad_mode="constant",
-        pad_constant_values=0,
-        label_patch_size=None,
-    ):
-        super().__init__(
-            input_img=input_img,
-            patch_size=patch_size,
-            resolution=resolution,
-            units=units,
-            pad_mode=pad_mode,
-            pad_constant_values=pad_constant_values,
-        )
-        self.stride = stride
-        self.label_patch_size = label_patch_size
-
-    def __next__(self):
-        raise NotImplementedError
-
-    def merge_patches(self, patches):
-        raise NotImplementedError
 
 
 class PointsPatchExtractor(PatchExtractor):
@@ -298,23 +241,13 @@ class PointsPatchExtractor(PatchExtractor):
             (self.patch_size[1] - 1) / 2
         )
 
-    def merge_patches(self, patches=None):
-        """Merge patch is not supported by :obj:`PointsPatchExtractor`.
-        Calling this function for :obj:`PointsPatchExtractor` will raise an error. This
-        overrides the merge_patches function in the base class :obj:`PatchExtractor`
-
-        """
-        raise MethodNotSupported(
-            message="Merge patches not supported for PointsPatchExtractor"
-        )
-
 
 def get_patch_extractor(method_name, **kwargs):
     """Return a patch extractor object as requested.
 
     Args:
-        method_name (str): name of patch extraction method, must be one of "point",
-          "fixedwindow", "variablewindow".
+        method_name (str): name of patch extraction method, must be one of "point" or
+          "slidingwindow".
         **kwargs: Keyword arguments passed to :obj:`PatchExtractor`.
 
     Returns:
@@ -329,10 +262,8 @@ def get_patch_extractor(method_name, **kwargs):
     """
     if method_name.lower() == "point":
         patch_extractor = PointsPatchExtractor(**kwargs)
-    elif method_name.lower() == "fixedwindow":
-        patch_extractor = FixedWindowPatchExtractor(**kwargs)
-    elif method_name.lower() == "variablewindow":
-        patch_extractor = VariableWindowPatchExtractor(**kwargs)
+    elif method_name.lower() == "slidingwindow":
+        patch_extractor = SlidingWindowPatchExtractor(**kwargs)
     else:
         raise MethodNotSupported
 
