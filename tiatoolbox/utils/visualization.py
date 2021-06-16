7c0c53d8c26219cdf30426e6e9296d0d8fd88047
vqdang <dangvuquoc1993@gmail.com>
2021-06-16 10:35:15 +0000

    UPD: update

--- tiatoolbox/utils/visualization.py
+++ tiatoolbox/utils/visualization.py
@@ -28,77 +28,85 @@ import matplotlib as mpl
 from tiatoolbox.utils.misc import get_pretrained_model_info
 
 
-def overlay_patch_prediction(img, prediction, pretrained_model, alpha=0.35):
+def overlay_patch_prediction(
+            img : np.ndarray,
+            prediction : np.ndarray,
+            alpha : float = 0.35,
+            label_info : dict = None,
+            cmap=None,
+            ax=None,
+        ):
     """Generate an overlay, given a 2D prediction map.
 
     Args:
         img (ndarray): Image to overlay the results on top of.
         prediction (ndarray): 2D prediction map. Multi-class prediction should have
             values ranging from 0 to N-1, where N is the number of classes.
-        pretrained_model (str): Pretrained model used for generating the predictions.
-            This is used to determine predefined RGB overlay colours.
+        label_info (dict): A dictionary contains the mapping for each integer value
+            within `prediction` to its string and color. [int] : (str, (int, int, int)).
+            By default, integer will be taken as label and color will be random.
         alpha (float): Opacity value used for the overlay.
 
     """
     img = img.astype("uint8")
     overlay = img.copy()
 
-    pretrained_model = pretrained_model.lower()
-    _, dataset = pretrained_model.split("-")
-    # get label/model information
-    pretrained_yml = get_pretrained_model_info()
-    # focus on specific dataset
-    if dataset in pretrained_yml:
-        pretrained_info = pretrained_yml[dataset]
-        # get a dictionary of label ID and overlay colour
-        label_dict = pretrained_info["overlay_info"]
-        class_names = list(pretrained_info["label_info"].values())
-    else:
+    # generate random colours
+    predicted_classes = sorted(np.unique(prediction).tolist())
+    if label_info is not None:
         np.random.seed(123)
-        # if pretrained_model is not provided, generate random colours
-        predicted_classes = sorted(np.unique(prediction).tolist())
-        label_dict = {}
-        for label in predicted_classes:
-            label_dict[label] = (np.random.choice(range(256), size=3)).astype("uint8")
-        class_names = None
+        label_info = {}
+        for label_uid in predicted_classes:
+            random_colour = np.random.choice(range(256), size=3)
+            label_info[label_uid] = (str(label_uid), random_colour)
+    else:
+        if len(predicted_classes) != len(label_info):
+            raise ValueError((
+                    'Label info does not match '
+                    'number of classes in prediction.'))
+        for label_uid, (label_name, label_colour) in label_info.items():
+            if not np.issubdtype(label_uid, np.interger):
+                raise ValueError('Wrong format')
+            if not isinstance(label_name, str):
+                raise ValueError('Wrong format')
+            if not isinstance(label_colour, [tuple, list, np.ndarray]):
+                raise ValueError('Wrong format')
+            if len(label_colour) != 3:
+                raise ValueError('Wrong format')
 
     rgb_prediction = np.zeros(
         [prediction.shape[0], prediction.shape[1], 3], dtype=np.uint8
     )
-    for label, overlay_rgb in label_dict.items():
-        prediction_ = prediction.copy()
-        prediction_single_class = prediction_ == label
-        prediction_single_class = np.dstack(
-            [prediction_single_class, prediction_single_class, prediction_single_class]
-        )
-        prediction_single_class = prediction_single_class.astype("uint8")
-        prediction_single_class *= np.array(overlay_rgb).astype("uint8")
-        rgb_prediction += prediction_single_class
+    for label_uid, (_, overlay_rgb) in label_info.items():
+        sel = prediction == label_uid
+        rgb_prediction[sel] = overlay_rgb
 
     # add the overlay
     cv2.addWeighted(rgb_prediction, alpha, overlay, 1 - alpha, 0, overlay)
-    overlay = overlay.astype("uint8")
+    overlay = overlay.astype(np.uint8)
 
     # create colorbar parameters
-    colors_list = np.array(list(label_dict.values()), dtype=np.float) / 255
-    bounds = list(label_dict.keys())
-    cmap = mpl.colors.ListedColormap(colors_list)
+    name_list = [v[0] for v in label_info.values()]
+    color_list = [v[1] for v in label_info.values()]
+    color_list = np.array(color_list) / 255
+    uid_list = list(label_info.keys())
+    cmap = mpl.colors.ListedColormap(color_list)
     colorbar_params = {
         "mappable": mpl.cm.ScalarMappable(cmap=cmap),
-        "boundaries": bounds + [bounds[-1] + 1],
-        "ticks": [b + 0.5 for b in bounds],
+        "boundaries": uid_list + [uid_list[-1] + 1],
+        "ticks": [b + 0.5 for b in uid_list],
         "spacing": "proportional",
         "orientation": "vertical",
     }
 
-    # generate overlay with colourbar
-    fig, ax = plt.subplots()
+    # generate another ax, else using the provided
+    if ax is not None:
+        fig, ax = plt.subplots()
     ax.imshow(overlay)
     ax.axis("off")
-    # only save colourbar if class names are available
-    if class_names is not None:
-        cbar = plt.colorbar(**colorbar_params)
-        cbar.ax.set_yticklabels(class_names)
-        cbar.ax.tick_params(labelsize=12)
+    # generate colour bar
+    cbar = plt.colorbar(**colorbar_params)
+    cbar.ax.set_yticklabels(name_list)
+    cbar.ax.tick_params(labelsize=12)
 
-    return fig
+    return ax
