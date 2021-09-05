bcfeb29cfe6913fbc2143d8f400cc37edca94821
shaneahmed <sa>
2021-09-05 10:26:32 +0100

    DEV: Update minimum requirement to python 3.7 - Update minimum requirement to python 3.7 - Add support for python 3.9

--- setup.py
+++ setup.py
@@ -37,16 +37,16 @@ test_requirements = [
 
 setup(
     author="TIA Lab",
-    author_email="tialab@dcs.warwick.ac.uk",
-    python_requires=">=3.6",
+    author_email="tia@dcs.warwick.ac.uk",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="Computational pathology toolbox developed by TIA Lab.",
     entry_points={
