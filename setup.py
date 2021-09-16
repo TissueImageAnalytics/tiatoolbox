995c3e01f60003788e213833693c5931727b57de
shaneahmed <sa>
2021-09-16 12:37:54 +0100

    REL: Bump version to 0.7.0 - Bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tialab/tiatoolbox",
-    version="0.6.0",
+    version="0.7.0",
     zip_safe=False,
 )
