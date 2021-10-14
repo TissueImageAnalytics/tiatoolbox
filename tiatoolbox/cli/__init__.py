e190a0b9aa77222c8334f7eca8f3e8cd08ca285a
Shan E Ahmed Raza <shaaneahmed@gmail.com>
2021-10-14 23:02:55 +0100

    DEV: Rearrange command line interface to simplify addition of new commands (#156)
    - Move all cli commands/options from cli.py to cli_commands to make it clean and easier to add new commands
    - Fix test in tests/test_slide_info
    - Move string_to_tuple to tiatoolbox/utils/misc.py
    - Fix docstring in patch_predictor.
    
    Co-authored by: @shaneahmed

---
+++ tiatoolbox/cli/__init__.py
@@ -0,0 +1,64 @@
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# The Original Code is Copyright (C) 2021, TIALab, University of Warwick
+# All rights reserved.
+# ***** END GPL LICENSE BLOCK *****
+
+"""Console script for tiatoolbox."""
+import os
+import sys
+
+import click
+
+from tiatoolbox import __version__
+from tiatoolbox.cli.patch_predictor import patch_predictor
+from tiatoolbox.cli.read_bounds import read_bounds
+from tiatoolbox.cli.save_tiles import save_tiles
+from tiatoolbox.cli.slide_info import slide_info
+from tiatoolbox.cli.slide_thumbnail import slide_thumbnail
+from tiatoolbox.cli.stain_norm import stain_norm
+from tiatoolbox.cli.tissue_mask import tissue_mask
+
+
+def version_msg():
+    """Return a string with tiatoolbox package version and python version."""
+    python_version = sys.version[:3]
+    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    message = "tiatoolbox %(version)s from {} (Python {})"
+    return message.format(location, python_version)
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.version_option(
+    __version__, "--version", "-V", help="Version", message=version_msg()
+)
+def main():
+    """Computational pathology toolbox by TIA LAB."""
+    return 0
+
+
+main.add_command(slide_info)
+main.add_command(read_bounds)
+main.add_command(slide_thumbnail)
+main.add_command(save_tiles)
+main.add_command(tissue_mask)
+main.add_command(patch_predictor)
+main.add_command(stain_norm)
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
---
+++ tiatoolbox/cli/slide_info.py
@@ -0,0 +1,87 @@
+"""Command line interface for slide_info."""
+import os
+import pathlib
+import sys
+
+import click
+
+from tiatoolbox import utils, wsicore
+
+
+@click.group()
+def main():  # pragma: no cover
+    """Define slide_info click group."""
+    return 0
+
+
+@main.command()
+@click.option("--img_input", help="input path to WSI file or directory path")
+@click.option(
+    "--output_dir",
+    help="Path to output directory to save the output, default=img_input/../meta",
+)
+@click.option(
+    "--file_types",
+    help="file types to capture from directory, default='*.ndpi', '*.svs', '*.mrxs'",
+    default="*.ndpi, *.svs, *.mrxs, *.jp2",
+)
+@click.option(
+    "--mode",
+    default="show",
+    help="'show' to display meta information only or 'save' to save "
+    "the meta information, default=show",
+)
+@click.option(
+    "--verbose",
+    type=bool,
+    default=True,
+    help="Print output, default=True",
+)
+def slide_info(img_input, output_dir, file_types, mode, verbose):
+    """Display or save WSI metadata."""
+    file_types = utils.misc.string_to_tuple(in_str=file_types)
+
+    if isinstance(output_dir, str):
+        output_dir = pathlib.Path(output_dir)
+
+    if not os.path.exists(img_input):
+        raise FileNotFoundError
+
+    files_all = [
+        img_input,
+    ]
+
+    if os.path.isdir(img_input):
+        files_all = utils.misc.grab_files_from_dir(
+            input_path=img_input, file_types=file_types
+        )
+
+    if output_dir is None and mode == "save":
+        input_dir = pathlib.Path(img_input).parent
+        output_dir = input_dir / "meta"
+
+    print(files_all)
+
+    if mode == "save":
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    for curr_file in files_all:
+        slide_param = wsicore.slide_info.slide_info(
+            input_path=curr_file, verbose=verbose
+        )
+        if mode == "show":
+            print(slide_param.as_dict())
+
+        if mode == "save":
+            out_path = pathlib.Path(
+                output_dir, slide_param.file_path.with_suffix(".yaml").name
+            )
+            utils.misc.save_yaml(
+                slide_param.as_dict(),
+                out_path,
+            )
+            print("Meta files saved at " + str(output_dir))
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
