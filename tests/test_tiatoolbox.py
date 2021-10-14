477053d9339502dd0d2287e16c2f347ee2087bec
Shan E Ahmed Raza <shaaneahmed@gmail.com>
2021-10-14 15:01:42 +0100

    TST: Add tests for cli_commands - Add tests for cli_commands for coverage

--- tests/test_tiatoolbox.py
+++ tests/test_tiatoolbox.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 """Pytests for `tiatoolbox` package."""
-from tiatoolbox import cli
-from tiatoolbox import __version__
-
 from click.testing import CliRunner
 
+from tiatoolbox import __version__, cli
+from tiatoolbox.cli_commands import (
+    patch_predictor,
+    read_bounds,
+    save_tiles,
+    slide_info,
+    slide_thumbnail,
+    tissue_mask,
+)
 
 # -------------------------------------------------------------------------------------
 # Command Line Interface
@@ -27,3 +33,39 @@ def test_command_line_version():
     runner = CliRunner()
     version_result = runner.invoke(cli.main, ["-V"])
     assert __version__ in version_result.output
+
+
+def test_cli_patch_predictor():
+    runner = CliRunner()
+    patch_predictor_result = runner.invoke(patch_predictor.main)
+    assert patch_predictor_result.exit_code == 0
+
+
+def test_cli_read_bounds():
+    runner = CliRunner()
+    read_bounds_result = runner.invoke(read_bounds.main)
+    assert read_bounds_result.exit_code == 0
+
+
+def test_cli_save_tiles():
+    runner = CliRunner()
+    save_tiles_result = runner.invoke(save_tiles.main)
+    assert save_tiles_result.exit_code == 0
+
+
+def test_cli_slide_info():
+    runner = CliRunner()
+    slide_info_result = runner.invoke(slide_info.main)
+    assert slide_info_result.exit_code == 0
+
+
+def test_cli_slide_thumbnail():
+    runner = CliRunner()
+    slide_thumbnail_result = runner.invoke(slide_thumbnail.main)
+    assert slide_thumbnail_result.exit_code == 0
+
+
+def test_cli_tissue_mask():
+    runner = CliRunner()
+    tissue_mask_result = runner.invoke(tissue_mask.main)
+    assert tissue_mask_result.exit_code == 0
