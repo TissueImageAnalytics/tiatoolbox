5d8223193f8466d4090c82ee142b041a028c8dc5
Shan E Ahmed Raza <shaaneahmed@gmail.com>
2021-10-14 14:17:04 +0100

    DEV: Move save_tiles from cli to cli_commands/save_tiles - Move save_tiles from cli to cli_commands/save_tiles

--- tiatoolbox/cli.py
+++ tiatoolbox/cli.py
@@ -29,6 +29,7 @@ from PIL import Image
 
 from tiatoolbox import __version__, utils, wsicore
 from tiatoolbox.cli_commands.read_bounds import read_bounds
+from tiatoolbox.cli_commands.save_tiles import save_tiles
 from tiatoolbox.cli_commands.slide_info import slide_info
 from tiatoolbox.cli_commands.slide_thumbnail import slide_thumbnail
 from tiatoolbox.models.controller.patch_predictor import CNNPatchPredictor
@@ -58,72 +59,7 @@ def main():
 main.add_command(slide_info)
 main.add_command(read_bounds)
 main.add_command(slide_thumbnail)
-
-
-@main.command()
-@click.option("--img_input", help="input path to WSI file or directory path")
-@click.option(
-    "--output_dir",
-    default="tiles",
-    help="Path to output directory to save the output, default=tiles",
-)
-@click.option(
-    "--file_types",
-    help="file types to capture from directory, default='*.ndpi', '*.svs', '*.mrxs'",
-    default="*.ndpi, *.svs, *.mrxs, *.jp2",
-)
-@click.option(
-    "--tile_objective_value",
-    type=int,
-    default=20,
-    help="objective value at which tile is generated- default=20",
-)
-@click.option(
-    "--tile_read_size",
-    type=int,
-    nargs=2,
-    default=[5000, 5000],
-    help="tile width, height default=5000 5000",
-)
-@click.option(
-    "--verbose",
-    type=bool,
-    default=True,
-    help="Print output, default=True",
-)
-def save_tiles(
-    img_input,
-    output_dir,
-    file_types,
-    tile_objective_value,
-    tile_read_size,
-    verbose=True,
-):
-    """Display or save WSI metadata."""
-    file_types = string_to_tuple(file_types=file_types)
-
-    if not os.path.exists(img_input):
-        raise FileNotFoundError
-
-    files_all = [
-        img_input,
-    ]
-
-    if os.path.isdir(img_input):
-        files_all = utils.misc.grab_files_from_dir(
-            input_path=img_input, file_types=file_types
-        )
-
-    print(files_all)
-
-    for curr_file in files_all:
-        wsicore.save_tiles.save_tiles(
-            input_path=curr_file,
-            output_dir=output_dir,
-            tile_objective_value=tile_objective_value,
-            tile_read_size=tile_read_size,
-            verbose=verbose,
-        )
+main.add_command(save_tiles)
 
 
 @main.command()
