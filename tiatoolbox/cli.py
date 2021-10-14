91d4e5025a55367323473d472531695b0586cb56
Shan E Ahmed Raza <shaaneahmed@gmail.com>
2021-10-14 14:21:09 +0100

    DEV: Move stain_norm from cli to cli_commands/stain_norm - Move stain_norm from cli to cli_commands/stain_norm

--- tiatoolbox/cli.py
+++ tiatoolbox/cli.py
@@ -33,7 +33,6 @@ from tiatoolbox.cli_commands.save_tiles import save_tiles
 from tiatoolbox.cli_commands.slide_info import slide_info
 from tiatoolbox.cli_commands.slide_thumbnail import slide_thumbnail
 from tiatoolbox.models.controller.patch_predictor import CNNPatchPredictor
-from tiatoolbox.tools import stainnorm as sn
 from tiatoolbox.tools import tissuemask
 from tiatoolbox.utils.exceptions import MethodNotSupported
 from tiatoolbox.utils.misc import string_to_tuple
@@ -60,73 +59,6 @@ main.add_command(slide_info)
 main.add_command(read_bounds)
 main.add_command(slide_thumbnail)
 main.add_command(save_tiles)
-
-
-@main.command()
-@click.option(
-    "--source_input",
-    help="input path to the source image or a directory of source images",
-)
-@click.option("--target_input", help="input path to the target image")
-@click.option(
-    "--method",
-    help="Stain normalisation method to use. Choose from 'reinhard', 'custom',"
-    "'ruifrok', 'macenko, 'vahadane'",
-    default="reinhard",
-)
-@click.option(
-    "--stain_matrix",
-    help="stain matrix to use in custom normaliser. This can either be a numpy array"
-    ", a path to a npy file or a path to a csv file. If using a path to a csv file, "
-    "there must not be any column headers.",
-    default=None,
-)
-@click.option(
-    "--output_path",
-    help="Output directory for stain normalisation",
-    default="stainorm_output",
-)
-@click.option(
-    "--file_types",
-    help="file types to capture from directory"
-    "default='*.png', '*.jpg', '*.tif', '*.tiff'",
-    default="*.png, *.jpg, *.tif, *.tiff",
-)
-def stain_norm(
-    source_input, target_input, method, stain_matrix, output_path, file_types
-):
-    """Stain normalise an input image/directory of input images."""
-    file_types = string_to_tuple(file_types=file_types)
-
-    if not os.path.exists(source_input):
-        raise FileNotFoundError
-
-    files_all = [
-        source_input,
-    ]
-
-    if os.path.isdir(source_input):
-        files_all = utils.misc.grab_files_from_dir(
-            input_path=source_input, file_types=file_types
-        )
-
-    if method not in ["reinhard", "custom", "ruifrok", "macenko", "vahadane"]:
-        raise MethodNotSupported
-
-    # init stain normalisation method
-    norm = sn.get_normaliser(method, stain_matrix)
-
-    # get stain information of target image
-    norm.fit(utils.misc.imread(target_input))
-
-    if not os.path.isdir(output_path):
-        os.makedirs(output_path)
-
-    for curr_file in files_all:
-        basename = os.path.basename(curr_file)
-        # transform source image
-        transform = norm.transform(utils.misc.imread(curr_file))
-        utils.misc.imwrite(os.path.join(output_path, basename), transform)
 
 
 @main.command()  # noqa: CCR001
