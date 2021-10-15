29c729f1baba3bbf1dfdd940c4ed3d15057b9b59
John Pocock <John-P@users.noreply.github.com>
2021-10-15 11:18:36 +0100

    TST: Update Test Imports

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -6,33 +6,33 @@ import torch
 
 from tiatoolbox.tools.graph import (
     affinity_to_edge_index,
-    build_graph_dict,
-    connect_points,
+    hybrid_clustered_graph,
+    delaunay_adjacency,
 )
 
 
-def test_connect_points_dthresh_type():
+def test_delaunay_adjacency_dthresh_type():
     """Test empty input raises a TypeError if dthresh is not a Number."""
     with pytest.raises(TypeError, match="number"):
-        connect_points(points=[[0, 0]], dthresh=None)
+        delaunay_adjacency(points=[[0, 0]], dthresh=None)
 
 
-def test_connect_points_empty():
+def test_delaunay_adjacency_empty():
     """Test empty input raises a ValueError."""
     points = np.array([])
     with pytest.raises(ValueError, match="Points must have length >= 4"):
-        connect_points(points)
+        delaunay_adjacency(points)
 
 
-def test_connect_points_invalid_shape():
+def test_delaunay_adjacency_invalid_shape():
     """Test points with invalid shape (not NxM) raises a ValueError."""
     points = np.random.rand(4, 4, 4)
     with pytest.raises(ValueError, match="NxM"):
-        connect_points(points)
+        delaunay_adjacency(points)
 
 
-def test_connect_points_nothing_connected():
-    """Test connect_points does not connect points further than dthresh.
+def test_delaunay_adjacency_nothing_connected():
+    """Test delaunay_adjacency does not connect points further than dthresh.
 
     Nothing should connect for this case as all points are further
     apart than dthresh.
@@ -46,12 +46,12 @@ def test_connect_points_nothing_connected():
             [1, 6],
         ]
     )
-    adjacency_matrix = connect_points(points=points, dthresh=0.5)
+    adjacency_matrix = delaunay_adjacency(points=points, dthresh=0.5)
     assert np.sum(adjacency_matrix) == 0
 
 
-def test_connect_points_connected():
-    """Test connect_points connects expects points in handcrafted input."""
+def test_delaunay_adjacency_connected():
+    """Test delaunay_adjacency connects expects points in handcrafted input."""
     # Simple convex hull with the minimum of 4 points
     points = np.array(
         [
@@ -61,7 +61,7 @@ def test_connect_points_connected():
             [1, 6],
         ]
     )
-    adjacency_matrix = connect_points(points=points, dthresh=1.5)
+    adjacency_matrix = delaunay_adjacency(points=points, dthresh=1.5)
     # Expect 1 connection, symmetrical so dividing by 2
     assert np.sum(adjacency_matrix) / 2 == 1
 
@@ -90,10 +90,10 @@ def test_affinity_to_edge_fuzz_output_shape():
         assert 0 <= m <= n ** 2
 
 
-def test_build_graph_dict():
+def test_hybrid_clustered_graph():
     points = np.concatenate([np.random.rand(25, 2) + offset for offset in range(4)])
     features = np.concatenate([np.random.rand(25, 100) + offset for offset in range(4)])
-    graph = build_graph_dict(points, features, lambda_h=0.001)
+    graph = hybrid_clustered_graph(points, features, lambda_h=0.001)
     x = graph["x"]
     assert len(x) > 0
     assert len(x) <= len(points)
