a58a087a2b737a9be9d7b7fdc773c9e06f1ddfb2
Shan E Ahmed Raza <shaaneahmed@gmail.com>
2021-10-22 15:12:52 +0100

    BUG: Fix cv2 1x1 resizing error - Fix cv2 1x1 resizing error

--- tests/models/test_abc.py
+++ tests/models/test_abc.py
@@ -22,13 +22,17 @@
 
 import pytest
 
+from tiatoolbox import rcParam
 from tiatoolbox.models.abc import ModelABC
+from tiatoolbox.models.architecture import get_pretrained_model
 
-# def test_get_pretrained_model():
-#     """Test for downloading and creating pretrained models."""
-#     pretrained_info = rcParam["pretrained_model_info"]
-#     for pretrained_name in pretrained_info.keys():
-#         get_pretrained_model(pretrained_name, overwrite=True)
+
+@pytest.mark.skip(reason="Local test, not applicable for travis.")
+def test_get_pretrained_model():
+    """Test for downloading and creating pretrained models."""
+    pretrained_info = rcParam["pretrained_model_info"]
+    for pretrained_name in pretrained_info.keys():
+        get_pretrained_model(pretrained_name, overwrite=True)
 
 
 def test_model_abc():
