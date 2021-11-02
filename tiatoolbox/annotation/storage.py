e721c9825a412cab22805d63652babf62328dc06
John Pocock <j.c.pocock@warwick.ac.uk>
2021-11-02 13:49:50 +0000

    DEV: Remove noqa Conflicting With skipcq

--- tiatoolbox/annotation/storage.py
+++ tiatoolbox/annotation/storage.py
@@ -232,7 +232,7 @@ class AnnotationStoreABC(ABC):
         if isinstance(properties_predicate, bytes):
             properties_predicate = pickle.loads(
                 properties_predicate
-            )  # noqa: SC100 skipcq: BAN-B301
+            )  # skipcq: BAN-B301
         return bool(properties_predicate(properties))
 
     def query(
@@ -564,7 +564,7 @@ class SQLiteStore(AnnotationStoreABC):
             return self._geometry_predicate(name, a, b)
 
         def pickle_properties_predicate(pickle_bytes: bytes, properties: str) -> bool:
-            fn = pickle.loads(pickle_bytes)  # noqa: SC100 skipcq: BAN-B301
+            fn = pickle.loads(pickle_bytes)  # skipcq: BAN-B301
             properties = json.loads(properties)
             return fn(properties)
 
