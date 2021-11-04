d864e2b2041bd5b36e22f1ab14c687ec4ab9956d
vqdang <dangvuquoc1993@gmail.com>
2021-11-04 15:02:40 +0000

    UPD: fix license

--- tests/models/test_semantic_segmentation.py
+++ tests/models/test_semantic_segmentation.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
-# The Original Code is Copyright (C) 2021, TIALab, University of Warwick
+# The Original Code is Copyright (C) 2021, TIA Centre, University of Warwick
 # All rights reserved.
 # ***** END GPL LICENSE BLOCK *****
 """Tests for Semantic Segmentor."""
