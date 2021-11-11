68e98561cf380c3c715b3a0f86e8f9c01c415411
adamshephard <adam.shephard@warwick.ac.uk>
2021-11-11 16:58:28 +0000

    UPD: Updated unit testing.

--- tests/models/test_hovernet_plus.py
+++ tests/models/test_hovernet_plus.py
@@ -34,10 +34,25 @@ def test_functionality(remote_sample, tmp_path):
     """Functionality test."""
     tmp_path = str(tmp_path)
     sample_patch = str(remote_sample("stainnorm-source"))
-    patch = imread(sample_patch)
-    patch = imresize(patch, scale_factor=0.5)
-    patch = patch[0:256, 0:256]
+    patch_pre = imread(sample_patch)
+    patch_pre = imresize(patch_pre, scale_factor=0.5)
+    patch = patch_pre[0:256, 0:256]
     batch = torch.from_numpy(patch)[None]
+
+    # Test functionality with both nuclei segmentation and classification alone
+    model = HoVerNetPlus(num_types=6, num_layers=None, mode="fast")
+    fetch_pretrained_weights("hovernet_fast-pannuke", f"{tmp_path}/weigths.pth")
+    pretrained = torch.load(f"{tmp_path}/weigths.pth")
+    model.load_state_dict(pretrained)
+    # Test decoder as expected
+    assert len(model.decoder["np"]) > 0, "Decoder must contain np branch."
+    assert len(model.decoder["hv"]) > 0, "Decoder must contain hv branch."
+    assert len(model.decoder["tp"]) > 0, "Decoder must contain tp branch."
+    output = model.infer_batch(model, batch, on_gpu=False)
+    assert len(output) == 3, "Must contain predictions for: np, hv and tp branches."
+    output = [v[0] for v in output]
+    output = model.postproc(output)
+    assert len(output[1]) > 0, "Must have some nuclei."
 
     # Test functionality with both nuclei and layer segmentation
     model = HoVerNetPlus(num_types=3, num_layers=5, mode="fast")
@@ -55,37 +70,9 @@ def test_functionality(remote_sample, tmp_path):
     output = model.postproc(output)
     assert len(output[1]) > 0 and len(output[3]) > 0, "Must have some nuclei/layers."
 
-    # Test functionality with both nuclei segmentation and classification alone
-    model = HoVerNetPlus(num_types=6, num_layers=None, mode="fast")
-    fetch_pretrained_weights("hovernet_fast-pannuke", f"{tmp_path}/weigths.pth")
-    pretrained = torch.load(f"{tmp_path}/weigths.pth")
-    model.load_state_dict(pretrained)
-    # Test decoder as expected
-    assert len(model.decoder["np"]) > 0, "Decoder must contain np branch."
-    assert len(model.decoder["hv"]) > 0, "Decoder must contain hv branch."
-    assert len(model.decoder["tp"]) > 0, "Decoder must contain tp branch."
-    output = model.infer_batch(model, batch, on_gpu=False)
-    assert len(output) == 3, "Must contain predictions for: np, hv and tp branches."
-    output = [v[0] for v in output]
-    output = model.postproc(output)
-    assert len(output[1]) > 0, "Must have some nuclei."
-
-    # Test functionality with both nuclei segmentation and classification alone
-    model = HoVerNetPlus(num_types=6, num_layers=None, mode="original")
-    fetch_pretrained_weights("hovernet_original-pannuke", f"{tmp_path}/weigths.pth")
-    pretrained = torch.load(f"{tmp_path}/weigths.pth")
-    model.load_state_dict(pretrained)
-    output = model.infer_batch(model, batch, on_gpu=False)
-    assert len(output) == 3, "Must contain predictions for: np, hv and tp branches."
-    output = [v[0] for v in output]
-    output = model.postproc(output)
-    assert len(output[1]) > 0, "Must have some nuclei."
-
     # Test functionality with nuclei segmentation alone
     model = HoVerNetPlus(num_types=None, num_layers=None, mode="fast")
-    fetch_pretrained_weights("hovernet_fast-pannuke", f"{tmp_path}/weigths.pth")
-    pretrained = torch.load(f"{tmp_path}/weigths.pth")
-    model.load_state_dict(pretrained)
+    model.load_state_dict(pretrained, strict=False)
     # Test decoder as expected
     assert len(model.decoder["np"]) > 0, "Decoder must contain np branch."
     assert len(model.decoder["hv"]) > 0, "Decoder must contain hv branch."
@@ -97,16 +84,14 @@ def test_functionality(remote_sample, tmp_path):
 
     # Test functionality with layer segmentation alone
     model = HoVerNetPlus(num_types=None, num_layers=5, mode="fast")
-    fetch_pretrained_weights("hovernet-oed", f"{tmp_path}/weigths.pth")
-    pretrained = torch.load(f"{tmp_path}/weigths.pth")
-    model.load_state_dict(pretrained)
+    model.load_state_dict(pretrained, strict=False)
     # Test decoder as expected
     assert len(model.decoder["ls"]) > 0, "Decoder must contain ls branch."
     output = model.infer_batch(model, batch, on_gpu=False)
     assert len(output) > 0, "Must contain predictions for: ls branch."
     output = output[0][np.newaxis, :, :]
     output = model.postproc(output)
-    assert len(output[1]) > 0, "Must have some layers."
+    assert len(output[3]) > 0, "Must have some layers."
 
     # test crash when providing exotic mode
     with pytest.raises(ValueError, match=r".*Invalid mode.*"):
