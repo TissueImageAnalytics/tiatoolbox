3dc26998a76221b19cf9a79c856ffd1545d63e4b
simongraham <s.graham.1@warwick.ac.uk>
2021-11-21 21:24:48 +0000

    MAINT: Update model weights and base CNN for IDaRS

--- tiatoolbox/models/architecture/idars.py
+++ tiatoolbox/models/architecture/idars.py
@@ -22,13 +22,9 @@
 import numpy as np
 from torchvision import transforms
 
-from tiatoolbox import rcParam
 from tiatoolbox.models.architecture.vanilla import CNNModel
-from tiatoolbox.tools.stainnorm import get_normaliser
-from tiatoolbox.utils.misc import download_data, imread
 
-# fit Vahadane stain normalisation
-TARGET_URL = "https://tiatoolbox.dcs.warwick.ac.uk/models/idars/target.jpg"
+
 TRANSFORM = transforms.Compose(
     [
         transforms.ToTensor(),
@@ -36,17 +32,9 @@ TRANSFORM = transforms.Compose(
     ]
 )
 
-download_data(TARGET_URL, save_path=f"{rcParam['TIATOOLBOX_HOME']}/idars_target.jpg")
-TARGET = imread(f"{rcParam['TIATOOLBOX_HOME']}/idars_target.jpg")
-STAIN_NORMALIZER = get_normaliser(method_name="macenko")
-STAIN_NORMALIZER.fit(TARGET)
 
-
-class CNNTumor(CNNModel):
-    """Retrieve the model and add custom preprocessing.
-
-    This is named CNNModel1 in line with the original IDaRS paper and
-    is used for tumour segmentation.
+class CNNIDaRS(CNNModel):
+    """Retrieve the model and add custom preprocessing used in IDaRS paper.
 
     Args:
         backbone (str): Model name.
@@ -71,55 +59,6 @@ class CNNTumor(CNNModel):
         """
         img = img.copy()
         img = TRANSFORM(img)
-        # toTensor will turn image to CHW so we transpose again
-        img = img.permute(1, 2, 0)
-
-        return img
-
-
-class CNNMutation(CNNModel):
-    """Retrieve the model and add custom preprocessing, including
-      Vahadane stain normalisation.
-
-      This is named CNNModel2 in line with the original IDaRS paper and
-      is used for prediction of molecular pathways and key mutations.
-
-    Args:
-        backbone (str): Model name.
-        num_classes (int): Number of classes output by model.
-
-    """
-
-    def __init__(self, backbone, num_classes=1):
-        super().__init__(backbone, num_classes=num_classes)
-
-    @staticmethod
-    # skipcq: PYL-W0221
-    def preproc(img: np.ndarray):
-        """Define preprocessing steps.
-
-        Args:
-            img (np.ndarray): An image of shape HWC.
-
-        Return:
-            img (torch.Tensor): An image of shape HWC.
-
-        """
-        img = img.copy()
-
-        # apply stain normalisation
-        try:
-            stain_normed = STAIN_NORMALIZER.transform(img)
-        # skipcq:
-        except:  # noqa
-            # bad behavior when encountering blank image
-            # which leads to numerical error problems,
-            # we do not stain-normalize these cases
-            stain_normed = img
-        stain_normed = STAIN_NORMALIZER.transform(img)
-        img = TRANSFORM(stain_normed)
-
-        # img = TRANSFORM(img)
         # toTensor will turn image to CHW so we transpose again
         img = img.permute(1, 2, 0)
 
