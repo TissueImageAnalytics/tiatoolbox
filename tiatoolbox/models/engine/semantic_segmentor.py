4214f2f294454abe151900be4a929bd3f51a1ff7
vqdang <dangvuquoc1993@gmail.com>
2021-12-08 16:46:02 +0000

    [UPD]: minor fix

--- tiatoolbox/models/engine/semantic_segmentor.py
+++ tiatoolbox/models/engine/semantic_segmentor.py
@@ -402,7 +402,7 @@ class SemanticSegmentor:
         self,
         batch_size: int = 8,
         num_loader_workers: int = 0,
-        num_postproc_workers: int = None,
+        num_postproc_workers: int = 0,
         model: torch.nn.Module = None,
         pretrained_model: str = None,
         pretrained_weights: str = None,
@@ -440,7 +440,7 @@ class SemanticSegmentor:
         self.pretrained_model = pretrained_model
         self.batch_size = batch_size
         self.num_loader_workers = num_loader_workers
-        self.num_postproc_workers = num_postproc_workers
+        self.num_postproc_workers = None
         self.verbose = verbose
         self.auto_generate_mask = auto_generate_mask
 
