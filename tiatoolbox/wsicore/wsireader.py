1ec50dc70782278b1079f4a38036564ce2649f1f
John Pocock <John-P@users.noreply.github.com>
2021-12-20 12:05:58 +0000

    BUG: Fix Import Path

--- tiatoolbox/wsicore/wsireader.py
+++ tiatoolbox/wsicore/wsireader.py
@@ -40,7 +40,7 @@ from defusedxml.ElementTree import fromstring as et_from_string
 
 from tiatoolbox import utils
 from tiatoolbox.tools import tissuemask
-from tiatoolbox.utils.detection import pixman_warning
+from tiatoolbox.utils.env_detection import pixman_warning
 from tiatoolbox.utils.exceptions import FileNotSupported
 from tiatoolbox.wsicore.wsimeta import WSIMeta
 
