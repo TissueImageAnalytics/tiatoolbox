369a18549fe76e10d9c807e4e2ca34662c59ad85
John Pocock <John-P@users.noreply.github.com>
2022-02-04 12:07:39 +0000

    MAIN: Update DataLoader Error For Unsupported File Exts

--- tiatoolbox/models/dataset/abc.py
+++ tiatoolbox/models/dataset/abc.py
@@ -136,7 +136,7 @@ class PatchDatasetABC(ABC, torch.utils.data.Dataset):
         if path.suffix in (".npy", ".jpg", ".jpeg", ".tif", ".tiff", ".png"):
             patch = imread(path, as_uint8=False)
         else:
-            raise ValueError(f"Can not load data of `{path.suffix}`")
+            raise ValueError(f"Cannot load image data from `{path.suffix}` files.")
         return patch
 
     @staticmethod
