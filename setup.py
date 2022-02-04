2a475b800950dccff8708a8a06086e7bcfea00a8
John Pocock <John-P@users.noreply.github.com>
2022-02-04 11:12:59 +0000

    DEV: Bring Dependencies In Line With Conda-Forge Release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
     "pillow",
     "matplotlib",
     "opencv-python>=4.5.4",
-    "openslide-python==1.1.2",
+    "openslide-python>=1.1.2",
     "pyyaml>=5.1",
     "pandas",
     "glymur",
