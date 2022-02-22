e4c2733be7e4b94bd8006834b49fd8097f510cbf
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-02-22 12:07:44 +0000

    BUG: Skip potential bug error - Skip potential bug error

--- tiatoolbox/models/architecture/micronet.py
+++ tiatoolbox/models/architecture/micronet.py
@@ -360,7 +360,7 @@ class MicroNet(ModelABC):
 
         self.apply(weights_init)
 
-    def forward(self, input_tensor: torch.Tensor, *args, **kwargs):
+    def forward(self, input_tensor: torch.Tensor):  # skipcq: PYL-W0221
         """Logic for using layers defined in init.
 
         This method defines how layers are used in forward operation.
