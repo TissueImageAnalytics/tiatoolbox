b983518ba4c222dd43893d96861357cb7c8eafdd
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-03-03 18:26:25 +0000

    DEV: Update cli using subclass. - Update cli using subclass.

--- tiatoolbox/cli/__init__.py
+++ tiatoolbox/cli/__init__.py
@@ -19,49 +19,70 @@
 # ***** END GPL LICENSE BLOCK *****
 
 """Console script for tiatoolbox."""
-import os
+import platform
 import sys
 
 import click
 
 from tiatoolbox import __version__
-from tiatoolbox.cli.nucleus_instance_segment import nucleus_instance_segment
-from tiatoolbox.cli.patch_predictor import patch_predictor
-from tiatoolbox.cli.read_bounds import read_bounds
-from tiatoolbox.cli.save_tiles import save_tiles
-from tiatoolbox.cli.semantic_segment import semantic_segment
-from tiatoolbox.cli.slide_info import slide_info
-from tiatoolbox.cli.slide_thumbnail import slide_thumbnail
-from tiatoolbox.cli.stain_norm import stain_norm
-from tiatoolbox.cli.tissue_mask import tissue_mask
+
+# from tiatoolbox.cli.nucleus_instance_segment import nucleus_instance_segment
+# from tiatoolbox.cli.patch_predictor import patch_predictor
+# from tiatoolbox.cli.read_bounds import read_bounds
+# from tiatoolbox.cli.save_tiles import save_tiles
+# from tiatoolbox.cli.semantic_segment import semantic_segment
+# from tiatoolbox.cli.slide_info import slide_info
+# from tiatoolbox.cli.slide_thumbnail import slide_thumbnail
+# from tiatoolbox.cli.stain_norm import stain_norm
+# from tiatoolbox.cli.tissue_mask import tissue_mask
 
 
 def version_msg():
     """Return a string with tiatoolbox package version and python version."""
     python_version = sys.version[:3]
-    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    message = "tiatoolbox %(version)s from {} (Python {})"
-    return message.format(location, python_version)
+    message = (
+        f"tiatoolbox {__version__} (Python {python_version}) on {platform.platform()}."
+    )
+    return message
 
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]})
-@click.version_option(
-    __version__, "--version", "-V", help="Version", message=version_msg()
+class TIAToolboxCLI(click.Group):
+    def __init__(self, **extra):
+        super(TIAToolboxCLI, self).__init__()
+        # params = list(extra.pop("params", None) or ())
+        # print(params)
+        # params.append(version_option)
+        # click.Group.__init__(self, params=params, **extra)
+
+
+cli = TIAToolboxCLI(
+    help="Computational pathology toolbox by TIA Centre.",
+    context_settings={"help_option_names": ["-h", "--help"]},
 )
-def main():
+
+# self.add_command(nucleus_instance_segment)
+# main.add_command(patch_predictor)
+# main.add_command(read_bounds)
+# main.add_command(save_tiles)
+# main.add_command(semantic_segment)
+# main.add_command(slide_info)
+# main.add_command(slide_thumbnail)
+# main.add_command(tissue_mask)
+# main.add_command(stain_norm)
+
+
+@cli.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.version_option(
+    __version__,
+    "--version",
+    "-v",
+    help="Show the tiatoolbox version",
+    message=version_msg(),
+)
+def main() -> None:
     """Computational pathology toolbox by TIA Centre."""
-    return 0
+    return None
 
-
-main.add_command(nucleus_instance_segment)
-main.add_command(patch_predictor)
-main.add_command(read_bounds)
-main.add_command(save_tiles)
-main.add_command(semantic_segment)
-main.add_command(slide_info)
-main.add_command(slide_thumbnail)
-main.add_command(tissue_mask)
-main.add_command(stain_norm)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
