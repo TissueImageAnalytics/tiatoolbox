28c32648bc1f3e842c7b241637fd25af290386e6
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-05-13 13:13:26 +0100

    DEV: Depreciate Support For Python 3.7. (#344)
    - Remove support for python 3.7 to allow using latest version of image codecs and some advanced features.
    
    Co-authored-by: Shan Raza <13048456+shaneahmed@users.noreply.github.com>
    Co-authored-by: John Pocock <John-P@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -49,13 +49,12 @@ test_requirements = [
 setup(
     author="TIA Centre",
     author_email="tia@dcs.warwick.ac.uk",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
