9309524e606c0c3e01a0d44e3cb6398f43920038
Ruqayya <awanruqayya2@gmail.com>
2022-06-18 20:06:17 +0100

    DFBR registration

---
+++ tiatoolbox/tools/registration/__init__.py
@@ -0,0 +1,15 @@
+"""Models package for the models implemented in tiatoolbox."""
+from tiatoolbox.models import abc, architecture, dataset
+from tiatoolbox.models.engine.nucleus_instance_segmentor import NucleusInstanceSegmentor
+from tiatoolbox.models.engine.patch_predictor import (
+    IOPatchPredictorConfig,
+    PatchDataset,
+    PatchPredictor,
+    WSIPatchDataset,
+)
+from tiatoolbox.models.engine.semantic_segmentor import (
+    DeepFeatureExtractor,
+    IOSegmentorConfig,
+    SemanticSegmentor,
+    WSIStreamDataset,
+)
---
+++ tiatoolbox/tools/registration/wsi_registration.py
@@ -0,0 +1,10 @@
+import torchvision
+from torchvision.models._utils import IntermediateLayerGetter
+
+class rigidRegistration():
+	def __init__(self):
+		self.patch_size = (224, 224)
+		self.Xscale, self.Yscale = [], []
+		model = torchvision.models.vgg16(True)
+		return_layers = {'16': 'block3_pool', '23': 'block4_pool', '30': 'block5_pool'}
+		self.FeatureExtractor = IntermediateLayerGetter(model.features, return_layers=return_layers)
