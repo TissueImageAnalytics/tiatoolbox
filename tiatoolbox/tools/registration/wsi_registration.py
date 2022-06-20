86677f20f80584445d03061298a2937f3422c96e
Ruqayya Awan <awanruqayya2@gmail.com>
2022-06-20 13:00:24 +0100

    Update tiatoolbox/tools/registration/wsi_registration.py
    Co-authored-by: John Pocock <John-P@users.noreply.github.com>

--- tiatoolbox/tools/registration/wsi_registration.py
+++ tiatoolbox/tools/registration/wsi_registration.py
@@ -5,7 +5,7 @@ from torchvision.models._utils import IntermediateLayerGetter
 class rigidRegistration:
     def __init__(self):
         self.patch_size = (224, 224)
-        self.Xscale, self.Yscale = [], []
+        self.x_scale, self.y_scale = [], []
         model = torchvision.models.vgg16(True)
         return_layers = {"16": "block3_pool", "23": "block4_pool", "30": "block5_pool"}
         self.FeatureExtractor = IntermediateLayerGetter(
