d6cbda02096c839860117a2aefd43b1ebee22760
vqdang <24943262+vqdang@users.noreply.github.com>
2022-06-20 14:18:46 +0100

    Update tiatoolbox/models/dataset/abc.py

--- tiatoolbox/models/dataset/abc.py
+++ tiatoolbox/models/dataset/abc.py
@@ -15,7 +15,7 @@ class PatchDatasetABC(ABC, torch.utils.data.Dataset):
         preproc_func:
             Preprocessing function used to transform the input data. If
             supplied, then `torch.Compose` will be used on the input
-            preproc. preproc is a list of torchvision or custom transforms for
+            `preproc`. `preproc` is a list of torchvision or custom transforms for
             preprocessing the image. The transforms will be applied in
             the order that they are given in the list. For more
             information, use the following link:
