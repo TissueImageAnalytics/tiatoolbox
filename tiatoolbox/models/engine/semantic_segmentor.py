7aed27aff2f11f2babf0b582443105356c893b63
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-06-23 14:56:04 +0100

    BUG: Try skipping above the statement - Try skipping above the statement

--- tiatoolbox/models/engine/semantic_segmentor.py
+++ tiatoolbox/models/engine/semantic_segmentor.py
@@ -1170,7 +1170,8 @@ class SemanticSegmentor:
             logging.info("--Ouput: %s", str(wsi_save_path))
         # prevent deep source check because this is bypass and
         # delegating error message
-        except Exception as err:  # skipcq: PYL-W0703  # noqa: PIE786
+        # skipcq: PYL-W0703  # noqa: E800
+        except Exception as err:  # noqa: PIE786
             if crash_on_exception:
                 raise err
             logging.error(err)
