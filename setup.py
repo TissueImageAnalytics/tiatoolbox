019fceb62fa680b9d6ddac77e1a3918ebfb9d300
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-07-07 17:54:50 +0100

    REL: Release 1.2.1 (#405)
    - Bump version: 1.2.0 → 1.2.1
    
    ### Major Updates and Feature Improvements
    - None
    
    ### Changes to API
    - None
    
    ### Bug Fixes and Other Changes
    - Fixes issues with dependencies
    	- Adds flask to dependencies.
    - Fixes missing file in the python package
    - Clarifies help string for show-wsi option
    
    
    ### Development related changes
    - Removes Travis CI
    	- GitHub Actions will be used instead.
    - Adds pre-commit hooks to check requirements consistency.
    - Adds GitHub Action to resolve conda environment checks on Windows and Ubuntu.

--- setup.py
+++ setup.py
@@ -78,6 +78,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/TissueImageAnalytics/tiatoolbox",
-    version="1.2.0",
+    version="1.2.1",
     zip_safe=False,
 )
