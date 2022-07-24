8191f7fd26ce48347c1749fcaceaa9c166a20b0d
Ruqayya Awan <18444369+ruqayya@users.noreply.github.com>
2022-07-24 23:57:16 +0100

    Preprocessing of an image pair for registration

---
+++ tests/test_wsi_registration.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytest
+
+from tiatoolbox.tools.registration.wsi_registration import preprocess
+
+
+def test_preprocess():
+    """Test for preprocessing/normalization of an image pair"""
+    fixed_img = np.random.randint(256, size=(256, 256))
+    moving_img = np.random.randint(256, size=(256, 256))
+    _, _ = preprocess(fixed_img, moving_img)
+
+    fixed_img = np.random.randint(256, size=(256, 256, 3))
+    moving_img = np.random.randint(256, size=(256, 256, 3))
+    with pytest.raises(
+            ValueError, match=r".*The input images should be grayscale images.*"
+    ):
+        _, _ = preprocess(fixed_img, moving_img)
---
+++ tiatoolbox/tools/registration/wsi_registration.py
@@ -0,0 +1,32 @@
+import numpy as np
+from skimage import exposure, filters, morphology
+
+def preprocess(fixed_img, moving_img):
+    """This function performs normalization to unify the appearance of fixed
+     and moving images
+
+    Args:
+            fixed_img (:class:`numpy.ndarray`):
+                    A grayscale fixed image
+            moving_img (:class:`numpy.ndarray`):
+                    A grayscale moving image
+
+    Returns:
+            :class:`numpy.ndarray`:
+                    A normalized grayscale fixed image
+            :class:`numpy.ndarray`:
+                    A normalized grayscale moving image
+
+    """
+    if len(fixed_img.shape) != 2 or len(moving_img.shape) != 2:
+        raise ValueError(f'{"The input images should be grayscale images."}')
+
+    moving_entropy, fixed_entropy = filters.rank.entropy(
+        moving_img, morphology.disk(3)
+    ), filters.rank.entropy(fixed_img, morphology.disk(3))
+    if np.mean(fixed_entropy) > np.mean(moving_entropy):
+        moving_img = exposure.match_histograms(moving_img, fixed_img)
+    else:
+        fixed_img = exposure.match_histograms(fixed_img, moving_img)
+
+    return fixed_img, moving_img
