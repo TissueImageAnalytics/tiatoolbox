eb3634f91be1d08e427ead22bc9a1f4af4dfbaf0
Ruqayya Awan <18444369+ruqayya@users.noreply.github.com>
2022-07-30 11:12:05 +0100

    Update tests/test_wsi_registration.py
    Co-authored-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tests/test_wsi_registration.py
+++ tests/test_wsi_registration.py
@@ -5,7 +5,7 @@ from tiatoolbox.tools.registration.wsi_registration import preprocess
 
 
 def test_preprocess():
-    """Test for preprocessing/normalization of an image pair"""
+    """Test for preprocessing/normalization of an image pair."""
     fixed_img = np.random.randint(256, size=(256, 256))
     moving_img = np.random.randint(256, size=(256, 256))
     _, _ = preprocess(fixed_img, moving_img)
