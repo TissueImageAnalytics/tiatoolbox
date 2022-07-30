7da96f032e653d9730547f29b7dbc1791a61959b
Ruqayya Awan <18444369+ruqayya@users.noreply.github.com>
2022-07-30 12:18:38 +0100

    Prealignment of an image pair using the center of mass

--- tests/test_wsi_registration.py
+++ tests/test_wsi_registration.py
@@ -9,6 +9,7 @@ def test_match_histograms():
     fixed_img = np.random.randint(256, size=(256, 256))
     moving_img = np.random.randint(256, size=(256, 256))
     _, _ = match_histograms(fixed_img, moving_img)
+    _, _ = match_histograms(fixed_img, moving_img, 3)
 
     fixed_img = np.random.randint(256, size=(256, 256, 3))
     moving_img = np.random.randint(256, size=(256, 256, 3))
@@ -16,3 +17,7 @@ def test_match_histograms():
             ValueError, match=r".*The input images should be grayscale images.*"
     ):
         _, _ = match_histograms(fixed_img, moving_img)
+
+    fixed_img = np.random.randint(256, size=(256, 256, 1))
+    moving_img = np.random.randint(256, size=(256, 256, 1))
+    _, _ = match_histograms(fixed_img, moving_img)
--- tiatoolbox/tools/registration/wsi_registration.py
+++ tiatoolbox/tools/registration/wsi_registration.py
@@ -1,36 +1,38 @@
 import numpy as np
 from skimage import exposure, filters, morphology
 
-def match_histograms(image_a, image_b, disk_size=3):
-    """Image normalization function.
+def match_histograms(image_a, image_b, disk_radius=3):
+	"""Image normalization function.
 
-    This function performs histogram equalization to unify the
-    appearance of an image pair.
+	This function performs histogram equalization to unify the
+	appearance of an image pair.
 
-    Args:
-        fixed_img (:class:`numpy.ndarray`):
-            A grayscale fixed image.
-        moving_img (:class:`numpy.ndarray`):
-            A grayscale moving image.
+	Args:
+		image_a (:class:`numpy.ndarray`):
+			A grayscale image.
+		image_b (:class:`numpy.ndarray`):
+			A grayscale image.
+		disk_radius (int):
+			The radius of the disk-shaped footprint.
 
-    Returns:
-        :class:`numpy.ndarray`:
-            A normalized grayscale fixed image.
-        :class:`numpy.ndarray`:
-            A normalized grayscale moving image.
+	Returns:
+		:class:`numpy.ndarray`:
+			A normalized grayscale image.
+		:class:`numpy.ndarray`:
+			A normalized grayscale image.
 
-    """
+	"""
 
-    image_a, moving_img = np.squeeze(image_a), np.squeeze(image_b)
-    if len(image_a.shape) == 3 or len(image_b.shape) == 3:
-    	raise ValueError("The input images should be grayscale images.")
+	image_a, image_b = np.squeeze(image_a), np.squeeze(image_b)
+	if len(image_a.shape) == 3 or len(image_b.shape) == 3:
+		raise ValueError("The input images should be grayscale images.")
 
-    moving_entropy, fixed_entropy = filters.rank.entropy(
-        image_b, morphology.disk(disk_size)
-    ), filters.rank.entropy(image_a, morphology.disk(disk_size))
-    if np.mean(fixed_entropy) > np.mean(moving_entropy):
-        image_b = exposure.match_histograms(image_b, image_a)
-    else:
-        image_a = exposure.match_histograms(image_a, image_b)
+	entropy_a, entropy_b = filters.rank.entropy(
+		image_a, morphology.disk(disk_radius)
+	), filters.rank.entropy(image_b, morphology.disk(disk_radius))
+	if np.mean(entropy_a) > np.mean(entropy_b):
+		image_b = exposure.match_histograms(image_b, image_a)
+	else:
+		image_a = exposure.match_histograms(image_a, image_b)
 
-    return image_a, image_b
+	return image_a, image_b
