b7f2258abebc8f0a2e8cf6f583b4693d5c029a1a
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-08-16 18:08:23 +0100

    DOC: Fix example gallery. - Fix example gallery.

--- docs/conf.py
+++ docs/conf.py
@@ -124,6 +124,11 @@ html_theme = "alabaster"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/sg_gallery.css",
+]
 
 # -- Options for HTMLHelp output ---------------------------------------
 
