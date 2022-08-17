53eebae7c9762aa8fcc14e57a51f45cba81a6066
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-08-17 09:14:03 +0100

    DOC: Try moving css file to _static. - Try moving css file to _static.

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,7 @@ html_static_path = ["_static"]
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    "css/sg_gallery.css",
+    "sg_gallery.css",
 ]
 
 # -- Options for HTMLHelp output ---------------------------------------
