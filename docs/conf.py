9d8b2fe079b462c7b6c902af987f6d8607e0b98d
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-09-09 09:31:40 +0100

    DEV: Add svg options to docs/conf.py - Add svg options to docs/conf.py
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -124,17 +124,21 @@ html_title = f"TIA Toolbox {tiatoolbox.__version__} Documentation"
 html_theme_options = {
     "navigation_with_keys": True,
     "top_of_page_button": None,
-    "announcement": "Paper available at "
-    "<a href='https://doi.org/10.1101/2021.12.23.474029'>"
-    "https://doi.org/10.1101/2021.12.23.474029</a>",
     "footer_icons": [
         {
             "name": "GitHub",
             "url": "https://github.com/TissueImageAnalytics/tiatoolbox",
-            "html": "",
-            "class": "fa-brands fa-github fa-2x",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
         },
     ],
+    "announcement": "Paper available at "
+    "<a href='https://doi.org/10.1101/2021.12.23.474029'>"
+    "https://doi.org/10.1101/2021.12.23.474029</a>",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
