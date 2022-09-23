75ec6b4d2608c91e18610baa0b1dc19e4ce5e6a6
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-09-23 16:12:31 +0100

    [skip ci] DOC: Add copy button to docs. - Add copy button to docs.
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
+    "sphinx_copybutton",
     "sphinx_toolbox.collapse",
     "myst_nb",
     "sphinx_design",
