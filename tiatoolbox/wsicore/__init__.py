2fb7b649604f2584d7e8c78eed71f2c97a65a39f
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-09-24 17:27:21 +0100

    BUG: Fix imports for wsicore - Fix imports for wsicore
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/wsicore/__init__.py
+++ tiatoolbox/wsicore/__init__.py
@@ -1,9 +1,2 @@
 """Package to read whole slide images"""
-from pathlib import Path
-
-from tiatoolbox import _lazy_import
-from tiatoolbox.wsicore import save_tiles, slide_info, wsimeta, wsireader
-
-location = Path(__file__).parent
-
-metadata = _lazy_import("metadata", location)
+from tiatoolbox.wsicore import metadata, save_tiles, slide_info, wsimeta, wsireader
