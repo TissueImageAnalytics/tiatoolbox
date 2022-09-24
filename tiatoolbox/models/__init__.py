5f45e53f73f7f15bba704cd66b498461a42f553d
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-09-24 17:48:27 +0100

    BUG: Fix imports for models - Fix imports for models
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/models/__init__.py
+++ tiatoolbox/models/__init__.py
@@ -1,15 +1,5 @@
 """Models package for the models implemented in tiatoolbox."""
-from pathlib import Path
-
-from tiatoolbox import _lazy_import
-
-location = Path(__file__).parent
-
-architecture = _lazy_import("architecture", location)
-dataset = _lazy_import("dataset", location)
-engine = _lazy_import("engine", location)
-models_abc = _lazy_import("models_abc", location)
-
+from tiatoolbox.models import architecture, dataset, engine, models_abc
 from tiatoolbox.models.engine.nucleus_instance_segmentor import NucleusInstanceSegmentor
 from tiatoolbox.models.engine.patch_predictor import (
     IOPatchPredictorConfig,
--- tiatoolbox/models/engine/__init__.py
+++ tiatoolbox/models/engine/__init__.py
@@ -1,9 +1,6 @@
-from pathlib import Path
-
-from tiatoolbox import _lazy_import
-
-location = Path(__file__).parent
-
-nucleus_instance_segmentor = _lazy_import("nucleus_instance_segmentor", location)
-patch_predictor = _lazy_import("patch_predictor", location)
-semantic_segmentor = _lazy_import("semantic_segmentor", location)
+"""Engines to run models implemented in tiatoolbox."""
+from tiatoolbox.models.engine import (
+    nucleus_instance_segmentor,
+    patch_predictor,
+    semantic_segmentor,
+)
