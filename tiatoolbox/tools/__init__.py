4c662b9157af02807eed5ef3e6236a2234fd3d89
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-09-24 17:32:09 +0100

    BUG: Fix imports for registration - Fix imports for registration
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/tools/__init__.py
+++ tiatoolbox/tools/__init__.py
@@ -1,16 +1,11 @@
 """This package contains various tools for working with WSIs."""
-from pathlib import Path
-
-from tiatoolbox import _lazy_import
 from tiatoolbox.tools import (
     graph,
     patchextraction,
     pyramid,
+    registration,
     stainaugment,
     stainextract,
     stainnorm,
     tissuemask,
 )
-
-location = Path(__file__).parent
-registration = _lazy_import("registration", location)
