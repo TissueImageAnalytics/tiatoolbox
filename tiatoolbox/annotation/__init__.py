6b32827fb0a31cea5993b5535f03722956bc37a4
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-09-24 17:00:11 +0100

    BUG: Fix imports for dsl - Fix imports for dsl
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/annotation/__init__.py
+++ tiatoolbox/annotation/__init__.py
@@ -1,8 +1,2 @@
 """Module initialisation."""
-from pathlib import Path
-
-from tiatoolbox import _lazy_import
-
-location = Path(__file__).parent
-dsl = _lazy_import("dsl", location)
-storage = _lazy_import("storage", location)
+from tiatoolbox.annotation import dsl, storage
