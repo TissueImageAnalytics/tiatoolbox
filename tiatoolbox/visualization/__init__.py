8123031128f0af1b0bc26f2a64d64973c5d4718d
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2022-09-24 17:40:37 +0100

    BUG: Fix imports for tileserver - Fix imports for tileserver
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/visualization/__init__.py
+++ tiatoolbox/visualization/__init__.py
@@ -1,7 +1,1 @@
-from pathlib import Path
-
-from tiatoolbox import _lazy_import
-
-location = Path(__file__).parent
-
-tileserver = _lazy_import("tileserver", location)
+from tiatoolbox.visualization import tileserver
