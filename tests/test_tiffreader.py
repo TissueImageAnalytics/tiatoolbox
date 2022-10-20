5f4234171ce73398114d879895a24538d3f5e146
John Pocock <John-P@users.noreply.github.com>
2022-10-20 21:37:03 +0100

    TST: Extract TIFFReader Specific Tests Into Module

---
+++ tests/test_tiffreader.py
@@ -0,0 +1,46 @@
+from defusedxml import ElementTree
+
+from tiatoolbox.wsicore import wsireader
+from tiatoolbox.data import _fetch_remote_sample
+
+
+def test_ome_missing_instrument_ref(monkeypatch):
+    """Test that an OME-TIFF can be read without instrument reference."""
+    sample = _fetch_remote_sample("ome-brightfield-pyramid-1-small")
+    wsi = wsireader.TIFFWSIReader(sample)
+    page = wsi.tiff.pages[0]
+    description = page.description
+    tree = ElementTree.fromstring(description)
+    namespaces = {
+        "ome": "http://www.openmicroscopy.org/Schemas/OME/2016-06",
+    }
+    images = tree.findall("ome:Image", namespaces)
+    for image in images:
+        instruments = image.findall("ome:InstrumentRef", namespaces)
+        for instrument in instruments:
+            image.remove(instrument)
+    new_description = ElementTree.tostring(tree, encoding="unicode")
+    monkeypatch.setattr(page, "description", new_description)
+    monkeypatch.setattr(wsi, "_m_info", None)
+    assert wsi.info.objective_power is None
+
+
+def test_ome_missing_physicalsize(monkeypatch):
+    """Test that an OME-TIFF can be read without physical size."""
+    sample = _fetch_remote_sample("ome-brightfield-pyramid-1-small")
+    wsi = wsireader.TIFFWSIReader(sample)
+    page = wsi.tiff.pages[0]
+    description = page.description
+    tree = ElementTree.fromstring(description)
+    namespaces = {
+        "ome": "http://www.openmicroscopy.org/Schemas/OME/2016-06",
+    }
+    images = tree.findall("ome:Image", namespaces)
+    for image in images:
+        pixels = image.find("ome:Pixels", namespaces)
+        del pixels.attrib["PhysicalSizeX"]
+        del pixels.attrib["PhysicalSizeY"]
+    new_description = ElementTree.tostring(tree, encoding="unicode")
+    monkeypatch.setattr(page, "description", new_description)
+    monkeypatch.setattr(wsi, "_m_info", None)
+    assert wsi.info.mpp is None
--- tests/test_wsireader.py
+++ tests/test_wsireader.py
@@ -1968,54 +1968,6 @@ def test_nff_no_scale_transforms_mpp(tmp_path):
     assert wsi.info.mpp is None
 
 
-class TestTIFFWSIReader:
-    @staticmethod
-    def test_ome_missing_instrument_ref(monkeypatch):
-        """Test that an OME-TIFF can be read without instrument reference."""
-        from defusedxml import ElementTree
-
-        sample = _fetch_remote_sample("ome-brightfield-pyramid-1-small")
-        wsi = wsireader.TIFFWSIReader(sample)
-        page = wsi.tiff.pages[0]
-        description = page.description
-        tree = ElementTree.fromstring(description)
-        namespaces = {
-            "ome": "http://www.openmicroscopy.org/Schemas/OME/2016-06",
-        }
-        images = tree.findall("ome:Image", namespaces)
-        for image in images:
-            instruments = image.findall("ome:InstrumentRef", namespaces)
-            for instrument in instruments:
-                image.remove(instrument)
-        new_description = ElementTree.tostring(tree, encoding="unicode")
-        monkeypatch.setattr(page, "description", new_description)
-        monkeypatch.setattr(wsi, "_m_info", None)
-        assert wsi.info.objective_power is None
-
-    @staticmethod
-    def test_ome_missing_physicalsize(monkeypatch):
-        """Test that an OME-TIFF can be read without physical size."""
-        from defusedxml import ElementTree
-
-        sample = _fetch_remote_sample("ome-brightfield-pyramid-1-small")
-        wsi = wsireader.TIFFWSIReader(sample)
-        page = wsi.tiff.pages[0]
-        description = page.description
-        tree = ElementTree.fromstring(description)
-        namespaces = {
-            "ome": "http://www.openmicroscopy.org/Schemas/OME/2016-06",
-        }
-        images = tree.findall("ome:Image", namespaces)
-        for image in images:
-            pixels = image.find("ome:Pixels", namespaces)
-            del pixels.attrib["PhysicalSizeX"]
-            del pixels.attrib["PhysicalSizeY"]
-        new_description = ElementTree.tostring(tree, encoding="unicode")
-        monkeypatch.setattr(page, "description", new_description)
-        monkeypatch.setattr(wsi, "_m_info", None)
-        assert wsi.info.mpp is None
-
-
 class TestReader:
     scenarios = [
         (
