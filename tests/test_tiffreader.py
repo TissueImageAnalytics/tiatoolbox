cc74589628d6393fa5ddc669acc69af158491373
John Pocock <John-P@users.noreply.github.com>
2022-10-21 13:17:14 +0100

    TST: Add Test For Missing OME PhysicalSizeY

--- tests/test_tiffreader.py
+++ tests/test_tiffreader.py
@@ -1,3 +1,4 @@
+import pytest
 from defusedxml import ElementTree
 
 from tiatoolbox.data import _fetch_remote_sample
@@ -44,3 +45,24 @@ def test_ome_missing_physicalsize(monkeypatch):
     monkeypatch.setattr(page, "description", new_description)
     monkeypatch.setattr(wsi, "_m_info", None)
     assert wsi.info.mpp is None
+
+
+def test_ome_missing_physicalsizey(monkeypatch):
+    """Test that an OME-TIFF can be read without physical size."""
+    sample = _fetch_remote_sample("ome-brightfield-pyramid-1-small")
+    wsi = wsireader.TIFFWSIReader(sample)
+    page = wsi.tiff.pages[0]
+    description = page.description
+    tree = ElementTree.fromstring(description)
+    namespaces = {
+        "ome": "http://www.openmicroscopy.org/Schemas/OME/2016-06",
+    }
+    images = tree.findall("ome:Image", namespaces)
+    for image in images:
+        pixels = image.find("ome:Pixels", namespaces)
+        del pixels.attrib["PhysicalSizeY"]
+    new_description = ElementTree.tostring(tree, encoding="unicode")
+    monkeypatch.setattr(page, "description", new_description)
+    monkeypatch.setattr(wsi, "_m_info", None)
+    with pytest.warns(UserWarning):
+        assert pytest.approx(wsi.info.mpp, abs=0.1) == 0.5
