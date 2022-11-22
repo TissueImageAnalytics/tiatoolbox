cd3b6664a5d97a4ac2d1fcad441c08150c53a829
Ruqayya Awan <18444369+ruqayya@users.noreply.github.com>
2022-11-22 14:25:55 +0000

    Update tiatoolbox/tools/registration/wsi_registration.py
    Co-authored-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/tools/registration/wsi_registration.py
+++ tiatoolbox/tools/registration/wsi_registration.py
@@ -1153,7 +1153,7 @@ def estimate_bspline_transform(
 
     Examples:
         >>> from tiatoolbox.tools.registration.wsi_registration import (
-        >>>     estimate_bspline_transform, apply_bspline_transform
+        ...     estimate_bspline_transform, apply_bspline_transform
         >>> )
         >>> bspline_transform = estimate_bspline_transform(
         >>>     fixed_gray_thumbnail, moving_gray_thumbnail, fixed_mask, moving_mask,
