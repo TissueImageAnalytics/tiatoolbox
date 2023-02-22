71cb5c6fd05e8f58aac5e8395290c9d8836d79de
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-02-22 09:50:10 +0000

    :white_check_mark: TST: Add test to improve coverage - Add test to improve coverage

--- tests/models/test_nucleus_detection_engine.py
+++ tests/models/test_nucleus_detection_engine.py
@@ -1,14 +1,33 @@
 """Tests for NucleusDetector."""
 
 import pathlib
+import shutil
 
 import pandas as pd
 import pytest
 
-from tiatoolbox.models.engine.nucleus_detector import NucleusDetector
+from tiatoolbox.models.engine.nucleus_detector import (
+    IONucleusDetectorConfig,
+    NucleusDetector,
+)
 from tiatoolbox.utils import env_detection as toolbox_env
 
 ON_GPU = not toolbox_env.running_on_ci() and toolbox_env.has_gpu()
+
+
+def _rm_dir(path):
+    """Helper func to remove directory."""
+    if pathlib.Path(path).exists():
+        shutil.rmtree(path, ignore_errors=True)
+
+
+def check_output(path):
+    """Check NucleusDetector output."""
+    coordinates = pd.read_csv(path)
+    assert coordinates.x[0] == pytest.approx(53, abs=2)
+    assert coordinates.x[1] == pytest.approx(55, abs=2)
+    assert coordinates.y[0] == pytest.approx(107, abs=2)
+    assert coordinates.y[1] == pytest.approx(127, abs=2)
 
 
 def test_nucleus_detector_engine(remote_sample, tmp_path):
@@ -23,8 +42,26 @@ def test_nucleus_detector_engine(remote_sample, tmp_path):
         on_gpu=ON_GPU,
     )
 
-    coordinates = pd.read_csv(tmp_path / "output" / "0.locations.0.csv")
-    assert coordinates.x[0] == pytest.approx(53, abs=2)
-    assert coordinates.x[1] == pytest.approx(55, abs=2)
-    assert coordinates.y[0] == pytest.approx(107, abs=2)
-    assert coordinates.y[1] == pytest.approx(127, abs=2)
+    check_output(tmp_path / "output" / "0.locations.0.csv")
+
+    _rm_dir(tmp_path / "output")
+
+    ioconfig = IONucleusDetectorConfig(
+        input_resolutions=[{"units": "mpp", "resolution": 0.5}],
+        output_resolutions=[{"units": "mpp", "resolution": 0.5}],
+        save_resolution=None,
+        patch_input_shape=[252, 252],
+        patch_output_shape=[252, 252],
+        stride_shape=[150, 150],
+    )
+
+    nucleus_detector = NucleusDetector(pretrained_model="mapde-conic")
+    _ = nucleus_detector.predict(
+        [mini_wsi_svs],
+        mode="wsi",
+        save_dir=tmp_path / "output",
+        on_gpu=ON_GPU,
+        ioconfig=ioconfig,
+    )
+
+    check_output(tmp_path / "output" / "0.locations.0.csv")
