215591f20c0a69e616e6d82f350f461ddf36701b
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-03-03 16:16:40 +0000

    :rotating_light: DEV: Use logger Instead Of print (`save_tiles.py`) (#490)
    - 🚨 Use logger instead of print in `save_tiles.py`.
    - 🔥 Remove save_tiles.py from wsicore
    - 🎨 Update cli/save_tiles to use wsi.save_tiles instead of save_tiles.py
    - 🎨 Fix logger to use lazy % formatting
    
    Co-authored-by: Shan Raza <13048456+shaneahmed@users.noreply.github.com>
    Co-authored-by: John Pocock <John-P@users.noreply.github.com>

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,13 +1,10 @@
 """Tests for exceptions used in the toolbox."""
 
-import pathlib
-
 import pytest
 
 from tiatoolbox import utils
 from tiatoolbox.tools.stainnorm import get_normalizer
 from tiatoolbox.utils.exceptions import FileNotSupported, MethodNotSupported
-from tiatoolbox.wsicore.save_tiles import save_tiles
 from tiatoolbox.wsicore.slide_info import slide_info
 
 
@@ -17,14 +14,6 @@ def test_exception_tests():
         utils.misc.save_yaml(
             slide_info(input_path="/mnt/test/sample.txt", verbose=True).as_dict(),
             "test.yaml",
-        )
-
-    with pytest.raises(FileNotSupported):
-        save_tiles(
-            input_path="/mnt/test/sample.txt",
-            tile_objective_value=5,
-            output_dir=str(pathlib.Path(__file__).parent.joinpath("tiles_save_tiles")),
-            verbose=True,
         )
 
     with pytest.raises(MethodNotSupported):
