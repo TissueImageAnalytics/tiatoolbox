52918577b169af0a114fd67f2d3282484dd1590b
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-03-10 15:45:35 +0000

    🚨 DEV: Use logger Instead Of print (slide_info.py) - 🚨 Use logger instead of print in `slide_info.py`. - 🔥 Remove slide-info.py from wsicore - 🎨 Update cli/slide_info to use wsi.slide_info instead of slide_info.py

--- tiatoolbox/cli/slide_info.py
+++ tiatoolbox/cli/slide_info.py
@@ -1,6 +1,8 @@
 """Command line interface for slide_info."""
+import logging
 import pathlib
 
+from tiatoolbox import logger
 from tiatoolbox.cli.common import (
     cli_file_type,
     cli_img_input,
@@ -20,7 +22,7 @@ from tiatoolbox.cli.common import (
 )
 @cli_file_type(default="*.ndpi, *.svs, *.mrxs, *.jp2")
 @cli_mode(default="show")
-@cli_verbose(default=True)
+@cli_verbose(default=False)
 def slide_info(img_input, output_path, file_types, mode, verbose):
     """Displays or saves WSI metadata depending on the mode argument."""
     from tiatoolbox import utils, wsicore
@@ -30,16 +32,22 @@ def slide_info(img_input, output_path, file_types, mode, verbose):
     )
 
     for curr_file in files_all:
-        slide_param = wsicore.slide_info.slide_info(
-            input_path=curr_file, verbose=verbose or mode == "show"
-        )
+        curr_file = pathlib.Path(curr_file)
+        wsi = wsicore.wsireader.WSIReader.open(input_img=curr_file)
+
+        if verbose:
+            logger.setLevel(logging.DEBUG)
+            logger.debug(curr_file.name)
+
+        if mode == "show":
+            logger.info(wsi.info.as_dict())
 
         if mode == "save":
             out_path = pathlib.Path(
-                output_path, slide_param.file_path.with_suffix(".yaml").name
+                output_path, wsi.info.file_path.with_suffix(".yaml").name
             )
             utils.misc.save_yaml(
-                slide_param.as_dict(),
+                wsi.info.as_dict(),
                 out_path,
             )
-            print("Meta files saved at " + str(output_path))
+            logger.info("Meta files saved at %s.", str(output_path))
