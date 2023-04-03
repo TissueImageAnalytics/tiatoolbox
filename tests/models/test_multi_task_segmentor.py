a9f1d0b49f24132c304b45b8b0b4f28c97117740
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-04-03 11:23:33 +0100

    🧪Fix failing tests - Fix failing tests

--- tests/models/test_multi_task_segmentor.py
+++ tests/models/test_multi_task_segmentor.py
@@ -18,7 +18,7 @@ from tiatoolbox.utils import env_detection as toolbox_env
 from tiatoolbox.utils.metrics import f1_detection
 from tiatoolbox.utils.misc import imwrite
 
-ON_GPU = toolbox_env.has_gpu()
+ON_GPU = False
 BATCH_SIZE = 1 if not ON_GPU else 8  # 16
 try:
     NUM_POSTPROC_WORKERS = multiprocessing.cpu_count()
@@ -33,7 +33,7 @@ def _rm_dir(path):
     shutil.rmtree(path, ignore_errors=True)
 
 
-def _crash_func():
+def _crash_func(_):
     """Helper to induce crash."""
     raise ValueError("Propagation Crash.")
 
