d4cf2e6f304f5a124ea2d0439021a0a7ac26e404
measty <20169086+measty@users.noreply.github.com>
2023-05-17 01:26:15 +0100

    test update

--- tests/test_tileserver.py
+++ tests/test_tileserver.py
@@ -467,7 +467,7 @@ def test_update_renderer(app):
         assert app.tia_pyramids["default"]["overlay"].renderer.blur_radius == 5
         assert app.overlaps["default"] == int(5 * 1.5)
 
-        response = client.put("/tileserver/renderer/where/None")
+        response = client.put(f"/tileserver/renderer/where/{json.dumps('None')}")
         assert app.tia_pyramids["default"]["overlay"].renderer.where is None
 
 
