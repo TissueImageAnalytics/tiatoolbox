25a9573c824cca3ab99556d3cd803ba0bd8f8d7c
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-05-19 11:00:49 +0100

    :art: Use `scikit-image` format - Use `scikit-image` format

--- tiatoolbox/models/__init__.py
+++ tiatoolbox/models/__init__.py
@@ -1,17 +1,5 @@
 """Models package for the models implemented in tiatoolbox."""
 from tiatoolbox.models import architecture, dataset, engine, models_abc
-from tiatoolbox.models.engine.patch_predictor import (
-    IOPatchPredictorConfig,
-    PatchDataset,
-    PatchPredictor,
-    WSIPatchDataset,
-)
-from tiatoolbox.models.engine.semantic_segmentor import (
-    DeepFeatureExtractor,
-    IOSegmentorConfig,
-    SemanticSegmentor,
-    WSIStreamDataset,
-)
 
 from .architecture.hovernet import HoVerNet
 from .architecture.hovernetplus import HoVerNetPlus
@@ -22,18 +10,29 @@ from .architecture.nuclick import NuClick
 from .architecture.sccnn import SCCNN
 from .engine.multi_task_segmentor import MultiTaskSegmentor
 from .engine.nucleus_instance_segmentor import NucleusInstanceSegmentor
-from .engine.patch_predictor import PatchPredictor
-from .engine.semantic_segmentor import SemanticSegmentor
+from .engine.patch_predictor import (
+    IOPatchPredictorConfig,
+    PatchDataset,
+    PatchPredictor,
+    WSIPatchDataset,
+)
+from .engine.semantic_segmentor import (
+    DeepFeatureExtractor,
+    IOSegmentorConfig,
+    SemanticSegmentor,
+    WSIStreamDataset,
+)
 
-HoVerNet = HoVerNet
-HoVerNetPlus = HoVerNetPlus
-IDaRS = IDaRS
-MapDe = MapDe
-MicroNet = MicroNet
-NuClick = NuClick
-SCCNN = SCCNN
-
-MultiTaskSegmentor = MultiTaskSegmentor
-NucleusInstanceSegmentor = NucleusInstanceSegmentor
-PatchPredictor = PatchPredictor
-SemanticSegmentor = SemanticSegmentor
+__all__ = [
+    "HoVerNet",
+    "HoVerNetPlus",
+    "IDaRS",
+    "MapDe",
+    "MicroNet",
+    "NuClick",
+    "SCCNN",
+    "MultiTaskSegmentor",
+    "NucleusInstanceSegmentor",
+    "PatchPredictor",
+    "SemanticSegmentor",
+]
