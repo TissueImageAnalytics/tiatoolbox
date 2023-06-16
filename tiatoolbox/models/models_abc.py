6926d81ab853dc18571574d9589acf1ec9a0f097
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-06-16 10:39:24 +0100

    [skip ci] :memo: Fix docstrings in models_abc.py - Fix docstrings in models_abc.py
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/models/models_abc.py
+++ tiatoolbox/models/models_abc.py
@@ -83,6 +83,7 @@ class ModelABC(ABC, nn.Module):
             >>> # expected usage
             >>> # model is a subclass object of this ModelABC
             >>> # `func` is a user defined function
+            >>> model = ModelABC()
             >>> model.preproc_func = func
             >>> transformed_img = model.preproc_func(img)
 
@@ -112,6 +113,7 @@ class ModelABC(ABC, nn.Module):
             >>> # expected usage
             >>> # model is a subclass object of this ModelABC
             >>> # `func` is a user defined function
+            >>> model = ModelABC()
             >>> model.postproc_func = func
             >>> transformed_img = model.postproc_func(img)
 
