70da5d33d5639f5adb510e81310f80632719dd84
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-06-17 00:19:23 +0100

    :recycle: Fix ioconfig import - Fix ioconfig import to avoid circular imports
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/models/engine/multi_task_segmentor.py
+++ tiatoolbox/models/engine/multi_task_segmentor.py
@@ -35,7 +35,7 @@ from tiatoolbox.models.engine.nucleus_instance_segmentor import (
     _process_instance_predictions,
 )
 
-from .. import IOInstanceSegmentorConfig
+from .io_config import IOInstanceSegmentorConfig
 from .semantic_segmentor import WSIStreamDataset
 
 
