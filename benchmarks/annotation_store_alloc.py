d2c329a43ae5dde3e674a1600c4a440606a8a5ae
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-06-29 16:50:31 +0100

    :bug: Fix docstrings - Fix docstrings
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- benchmarks/annotation_store_alloc.py
+++ benchmarks/annotation_store_alloc.py
@@ -122,6 +122,7 @@ except ImportError:
             """Dummy Tracker context manager."""
 
             def __init__(self, *args, **kwargs):
+                """Initialize :class:`Tracker`."""
                 warnings.warn("Memray not installed, skipping tracking.", stacklevel=2)
 
             def __enter__(self):
