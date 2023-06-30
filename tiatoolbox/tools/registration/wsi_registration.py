d6817db7e3fdc0113e6c8d85e555fd3050f8eb70
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-06-30 14:06:30 +0100

    :bug: Fix autofix errors - Fix autofix errors

--- tiatoolbox/tools/registration/wsi_registration.py
+++ tiatoolbox/tools/registration/wsi_registration.py
@@ -1005,10 +1005,10 @@ class DFBRegister:
                 block_quality,
             ) = self.feature_mapping(features)
             fixed_matched_points.append(
-                [*fixed_block_matched_points, bounding_box[2], bounding_box[0]],
+                fixed_block_matched_points + [bounding_box[2], bounding_box[0]],
             )
             moving_matched_points.append(
-                [*moving_block_matched_points, bounding_box[2], bounding_box[0]],
+                moving_block_matched_points + [bounding_box[2], bounding_box[0]],
             )
             quality.append(block_quality)
         fixed_matched_points, moving_matched_points, quality = (
