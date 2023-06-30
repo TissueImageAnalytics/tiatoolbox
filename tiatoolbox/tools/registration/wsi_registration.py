c9ecc5a72ab77f1f8446462b6ab39ebf35876124
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-06-30 21:51:16 +0100

    :bug: Fix logs - Fix logs
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tiatoolbox/tools/registration/wsi_registration.py
+++ tiatoolbox/tools/registration/wsi_registration.py
@@ -1346,7 +1346,7 @@ def estimate_bspline_transform(
         image1=fixed_image_inv_sitk,
         transformDomainMeshSize=mesh_size,
     )
-    logger.info("Initial Number of B-spline Parameters:", tx.GetNumberOfParameters)
+    logger.info(f"Initial Number of B-spline Parameters: {tx.GetNumberOfParameters()}")
 
     registration_method = sitk.ImageRegistrationMethod()
     registration_method.SetInitialTransformAsBSpline(
