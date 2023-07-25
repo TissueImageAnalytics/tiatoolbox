5dd9ff42a86e52f30b172bf92e80034430ae3916
John Pocock <John-P@users.noreply.github.com>
2023-07-25 16:23:17 +0100

    🐛 Remove TypeAlias for Python<3.10

--- tiatoolbox/typing.py
+++ tiatoolbox/typing.py
@@ -1,22 +1,22 @@
 """Define Variable types for TIAToolbox."""
-from typing import Callable, Literal, Sequence, SupportsFloat, TypeAlias
+from typing import Callable, Literal, Sequence, SupportsFloat
 
 import numpy as np
 from shapely.geometry import LineString, Point, Polygon
 
-JSON: TypeAlias = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
-NumPair: TypeAlias = tuple[SupportsFloat, SupportsFloat]
-IntPair: TypeAlias = tuple[int, int]
+JSON = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
+NumPair = tuple[SupportsFloat, SupportsFloat]
+IntPair = tuple[int, int]
 
 # WSIReader
-Resolution: TypeAlias = SupportsFloat | NumPair | np.ndarray | Sequence[SupportsFloat]
-Units: TypeAlias = Literal["mpp", "power", "baseline", "level"]
-Bounds: TypeAlias = tuple[SupportsFloat, SupportsFloat, SupportsFloat, SupportsFloat]
-IntBounds: TypeAlias = tuple[int, int, int, int]
+Resolution = SupportsFloat | NumPair | np.ndarray | Sequence[SupportsFloat]
+Units = Literal["mpp", "power", "baseline", "level"]
+Bounds = tuple[SupportsFloat, SupportsFloat, SupportsFloat, SupportsFloat]
+IntBounds = tuple[int, int, int, int]
 
 # Annotation Store
-Geometry: TypeAlias = Point | LineString | Polygon
-Properties: TypeAlias = JSON
+Geometry = Point | LineString | Polygon
+Properties = JSON
 QueryGeometry = Bounds | Geometry
 CallablePredicate = Callable[[Properties], bool]
 CallableSelect = Callable[[Properties], Properties]
