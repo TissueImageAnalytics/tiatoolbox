d009bbffc294d778bf95083eb7816cd63f3f726e
John Pocock <John-P@users.noreply.github.com>
2023-07-27 09:30:37 +0100

    🐛 Parse JP2 Boxes By ID Not Position (#651)
    JP2 Boxes can be in any order. 
    
    Previously the order was being relied on. This also allows non-Omnyx JP2 images which have a capture resolution box or manually specified MPP at init to be read as WSIs.
    
    Renames from "OmnyxJP2" to just "JP2" as this will work with any JP2 image. 
    
    # To-do
    
    - [x] Rename OmnyxJP2Reader -> JP2Reader?
    - [x] Use CMU-1-Small-Region.jp2 in tests instead of test1.jp2
    - [x] Resolve non-Omnyx file causing test failure.[^1] **Resolved:** allowing any occurrence of Aperio-style pipe-separated key-values for MPP = and AppMag = in an XML box \<description\> tag.
    
    [^1]: This is now intended behaviour. It was only passing by accident before. One of the test files is a non-Omnyx JP2, which happens to have an objective power in the description field, was picked up previously because the metadata is copied from an SVS file and has the same format as Omnyx. We must allow Aperio SVS-style metadata or change the test/test file to be Omnyx format.
    
    ---------
    
    Co-authored-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tests/test_slide_info.py
+++ tests/test_slide_info.py
@@ -55,7 +55,11 @@ def test_command_line_slide_info_jp2(sample_all_wsis):
     assert slide_info_result.exit_code == 0
     assert pathlib.Path(output_dir, "meta-data", "CMU-1-Small-Region.yaml").exists()
     assert pathlib.Path(output_dir, "meta-data", "CMU-1.yaml").exists()
-    assert pathlib.Path(output_dir, "meta-data", "test1.yaml").exists()
+    assert pathlib.Path(
+        output_dir,
+        "meta-data",
+        "CMU-1-Small-Region.omnyx.yaml",
+    ).exists()
 
 
 def test_command_line_slide_info_svs(sample_svs):
