83750ceb418bf64cbd238497c5abf7f42841605a
John Pocock <John-P@users.noreply.github.com>
2023-07-28 10:16:37 +0100

    ⏪ Add Removed Imports

--- tiatoolbox/utils/__init__.py
+++ tiatoolbox/utils/__init__.py
@@ -1,4 +1,13 @@
 """Utils package for toolbox utilities."""
+from tiatoolbox.utils import (
+    env_detection,
+    exceptions,
+    image,
+    metrics,
+    misc,
+    transforms,
+    visualization,
+)
 
 from .misc import download_data, imread, imwrite, save_as_json, save_yaml, unzip_data
 
