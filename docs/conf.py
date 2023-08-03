fcd1b5f160174d124edaeb0ecf728c0d886ca6ad
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-08-03 16:28:44 +0100

    :rotating_light: Fix import in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1996,7 +1996,7 @@ def all_but_ipynb(dir_path, contents):
 
 
 DOC_ROOT = os.path.dirname(os.path.realpath(__file__))
-PROJ_ROOT = pathlib.Path(DOC_ROOT).parent
+PROJ_ROOT = Path(DOC_ROOT).parent
 shutil.rmtree(os.path.join(PROJ_ROOT, "docs/_notebooks"), ignore_errors=True)
 shutil.copytree(
     os.path.join(PROJ_ROOT, "examples"),
