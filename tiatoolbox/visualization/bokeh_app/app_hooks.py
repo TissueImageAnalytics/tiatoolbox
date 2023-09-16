2a45be68f0e8adc6cbaf2ba51956b68456da56f9
measty <20169086+measty@users.noreply.github.com>
2023-09-17 00:07:49 +0100

    deepsource fix

--- tiatoolbox/visualization/bokeh_app/app_hooks.py
+++ tiatoolbox/visualization/bokeh_app/app_hooks.py
@@ -1,7 +1,13 @@
 """Hooks to be executed upon specific events in bokeh app."""
 import sys
 
+from tiatoolbox import logger
 
-def on_session_destroyed(session_context):  # noqa: ARG001, skipcq: PYL-W0613
+
+def on_session_destroyed(session_context):
     """Hook to be executed when a session is destroyed."""
+    logger.info(
+        "Session destroyed for user %s.",
+        session_context.request.arguments["user"],
+    )
     sys.exit()
