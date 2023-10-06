43371ebd1939100dcb895789c5ab30a7c4ff4b50
measty <20169086+measty@users.noreply.github.com>
2023-10-06 14:15:52 +0100

    annotations again

--- tiatoolbox/visualization/bokeh_app/app_hooks.py
+++ tiatoolbox/visualization/bokeh_app/app_hooks.py
@@ -3,10 +3,11 @@ import sys
 from contextlib import suppress
 
 import requests
+
 from bokeh.application.application import SessionContext
 
 
-def on_session_destroyed(session_context: SessionContext):
+def on_session_destroyed(session_context: SessionContext) -> None:
     """Hook to be executed when a session is destroyed."""
     user = session_context.request.arguments["user"]
     with suppress(requests.exceptions.ReadTimeout):
