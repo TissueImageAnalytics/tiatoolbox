bd134b0518ed2f6c3fdaad14eecf259d11e09dd0
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2023-10-10 14:42:56 +0100

    :bug: Fix `tiatoolbox/models/dataset/classification.py` for annotations

--- tiatoolbox/models/dataset/classification.py
+++ tiatoolbox/models/dataset/classification.py
@@ -1,7 +1,16 @@
 """Define classes and methods for classification datasets."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 import PIL
+from PIL import Image
 from torchvision import transforms
+
+from tiatoolbox.models.dataset import dataset_abc
+
+if TYPE_CHECKING:  # pragma: no cover
+    import numpy as np
 
 
 class _TorchPreprocCaller:
@@ -16,16 +25,16 @@ class _TorchPreprocCaller:
 
     """
 
-    def __init__(self, preprocs) -> None:
+    def __init__(self: _TorchPreprocCaller, preprocs: list) -> None:
         self.func = transforms.Compose(preprocs)
 
-    def __call__(self, img):
+    def __call__(self: _TorchPreprocCaller, img: np.ndarray) -> Image:
         img = PIL.Image.fromarray(img)
         img = self.func(img)
         return img.permute(1, 2, 0)
 
 
-def predefined_preproc_func(dataset_name):
+def predefined_preproc_func(dataset_name: str) -> _TorchPreprocCaller:
     """Get the preprocessing information used for the pretrained model.
 
     Args:
@@ -54,3 +63,67 @@ def predefined_preproc_func(dataset_name):
 
     preprocs = preproc_dict[dataset_name]
     return _TorchPreprocCaller(preprocs)
+
+
+class PatchDataset(dataset_abc.PatchDatasetABC):
+    """Define PatchDataset for torch inference.
+
+    Define a simple patch dataset, which inherits from the
+      `torch.utils.data.Dataset` class.
+
+    Attributes:
+        inputs (list or np.ndarray):
+            Either a list of patches, where each patch is a ndarray or a
+            list of valid path with its extension be (".jpg", ".jpeg",
+            ".tif", ".tiff", ".png") pointing to an image.
+        labels (list):
+            List of labels for sample at the same index in `inputs`.
+            Default is `None`.
+
+    Examples:
+        >>> # A user defined preproc func and expected behavior
+        >>> preproc_func = lambda img: img/2  # reduce intensity by half
+        >>> transformed_img = preproc_func(img)
+        >>> # create a dataset to get patches preprocessed by the above function
+        >>> ds = PatchDataset(
+        ...     inputs=['/A/B/C/img1.png', '/A/B/C/img2.png'],
+        ...     labels=["labels1", "labels2"],
+        ... )
+
+    """
+
+    def __init__(
+        self: PatchDataset,
+        inputs: np.ndarray | list,
+        labels: list | None = None,
+    ) -> None:
+        """Initialize :class:`PatchDataset`."""
+        super().__init__()
+
+        self.data_is_npy_alike = False
+
+        self.inputs = inputs
+        self.labels = labels
+
+        # perform check on the input
+        self._check_input_integrity(mode="patch")
+
+    def __getitem__(self: PatchDataset, idx: int) -> dict:
+        """Get an item from the dataset."""
+        patch = self.inputs[idx]
+
+        # Mode 0 is list of paths
+        if not self.data_is_npy_alike:
+            patch = self.load_img(patch)
+
+        # Apply preprocessing to selected patch
+        patch = self._preproc(patch)
+
+        data = {
+            "image": patch,
+        }
+        if self.labels is not None:
+            data["label"] = self.labels[idx]
+            return data
+
+        return data
