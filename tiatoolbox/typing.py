c957770540401cb25562d71f9de8eb131e7790ec
Jiaqi Lv <lvjiaqi9@gmail.com>
2023-11-10 12:13:57 +0000

    Revert back to using |

--- tiatoolbox/typing.py
+++ tiatoolbox/typing.py
@@ -1,26 +1,28 @@
 """Define Variable types for TIAToolbox."""
-from typing import Callable, Dict, List, Literal, Sequence, SupportsFloat, Tuple, Union
+from __future__ import annotations
+
+from typing import Callable, Dict, List, Literal, Sequence, SupportsFloat, Tuple
 
 import numpy as np
 from shapely.geometry import LineString, Point, Polygon  # type: ignore[import-untyped]
 
 # Proper type annotations for shapely is not yet avaliable
 
-JSON = Union[Dict[str, "JSON"], List["JSON"], str, int, float, bool, None]
+JSON = Dict[str, "JSON"] | List["JSON"] | str | int | float | bool | None
 NumPair = Tuple[SupportsFloat, SupportsFloat]
 IntPair = Tuple[int, int]
 
 # WSIReader
-Resolution = Union[SupportsFloat, NumPair, np.ndarray, Sequence[SupportsFloat]]
+Resolution = SupportsFloat | NumPair | np.ndarray | Sequence[SupportsFloat]
 Units = Literal["mpp", "power", "baseline", "level"]
 Bounds = Tuple[SupportsFloat, SupportsFloat, SupportsFloat, SupportsFloat]
 IntBounds = Tuple[int, int, int, int]
 
 # Annotation Store
-Geometry = Union[Point, LineString, Polygon]
+Geometry = Point | LineString | Polygon
 Properties = JSON
-QueryGeometry = Union[Bounds, Geometry]
+QueryGeometry = Bounds | Geometry
 CallablePredicate = Callable[[Properties], bool]
 CallableSelect = Callable[[Properties], Properties]
-Predicate = Union[str, bytes, CallablePredicate]
-Select = Union[str, bytes, CallableSelect]
+Predicate = str | bytes | CallablePredicate
+Select = str | bytes | CallableSelect
