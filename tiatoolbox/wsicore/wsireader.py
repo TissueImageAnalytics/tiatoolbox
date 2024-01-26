96018a6a7c7f2fc4168e89773ed0b74a31a5178b
Behnaz <behnazelhaminia@yahoo.com>
2024-01-26 09:34:25 +0000

    :bug: Fixed Line too long error

--- tiatoolbox/wsicore/wsireader.py
+++ tiatoolbox/wsicore/wsireader.py
@@ -2839,7 +2839,8 @@ class VirtualWSIReader(WSIReader):
         if mode != "bool" and (
             self.img.ndim == 2 or self.img.shape[2] not in [3, 4]  # noqa: PLR2004
         ):
-            logger.warning("The image mode is set to 'feature' as the input dimensions do not match with binary mask or RGB/RGBA.")
+            logger.warning("The image mode is set to 'feature' as the input"
+                           " dimensions do not match with binary mask or RGB/RGBA.")
             mode = "feature"
 
         self.mode = mode.lower()
