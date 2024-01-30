4c2a102be374ac01d9fcc09b33e84dd8b5151642
Abdol A <u2271662@live.warwick.ac.uk>
2024-01-30 14:08:14 +0000

    📝 Update test docstring

--- tests/models/test_patch_predictor.py
+++ tests/models/test_patch_predictor.py
@@ -1243,6 +1243,7 @@ def test_patch_predictor_torch_compile(
     Args:
         sample_patch1 (Path): Path to sample patch 1.
         sample_patch2 (Path): Path to sample patch 2.
+        tmp_path (Path): Path to temporary directory.
     """
     torch_compile_enabled = rcParam["enable_torch_compile"]
     rcParam["enable_torch_compile"] = True
