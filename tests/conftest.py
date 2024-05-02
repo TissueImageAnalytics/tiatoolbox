24f1cb5484d656ed77388e3af425629262462bb5
George Batchkala <george.batchkala@gmail.com>
2024-05-02 14:04:34 +0100

    add a fixture for downloading a remote sample: "non-tiled-ventana-tif"

--- tests/conftest.py
+++ tests/conftest.py
@@ -121,6 +121,16 @@ def sample_ome_tiff(remote_sample: Callable) -> Path:
 
 
 @pytest.fixture(scope="session")
+def sample_non_tiled_ventana_tif(remote_sample: Callable) -> Path:
+    """Sample pytest fixture for non-tiled tif Ventana images.
+
+    Download Ventana tif image for pytest.
+
+    """
+    return remote_sample("non-tiled-ventana-tif")
+
+
+@pytest.fixture(scope="session")
 def sample_jp2(remote_sample: Callable) -> Path:
     """Sample pytest fixture for JP2 images.
 
