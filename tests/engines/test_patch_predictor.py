72d4b8dda00348f4c5d2204d0c93e7d61bdf5a81
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2024-05-13 15:57:13 +0100

    :technologist: Add dummy tests.
    Signed-off-by: Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>

--- tests/engines/test_patch_predictor.py
+++ tests/engines/test_patch_predictor.py
@@ -2,76 +2,11 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, NoReturn
+from typing import NoReturn
 
 import pytest
 
 from tiatoolbox.models.engine.engine_abc import EngineABC
-
-if TYPE_CHECKING:
-    from pathlib import Path
-
-    import numpy as np
-    import torch.nn
-
-    from tiatoolbox.models.engine.io_config import ModelIOConfigABC
-
-
-class TestEngineABC(EngineABC):
-    """Test EngineABC."""
-
-    def __init__(
-        self: TestEngineABC,
-        model: str | torch.nn.Module,
-        weights: str | Path | None = None,
-        verbose: bool | None = None,
-    ) -> NoReturn:
-        """Test EngineABC init."""
-        super().__init__(model=model, weights=weights, verbose=verbose)
-
-    def get_dataloader(
-        self: EngineABC,
-        images: Path,
-        masks: Path | None = None,
-        labels: list | None = None,
-        ioconfig: ModelIOConfigABC | None = None,
-        *,
-        patch_mode: bool = True,
-    ) -> torch.utils.data.DataLoader:
-        """Test pre process images."""
-        return super().get_dataloader(
-            images,
-            masks,
-            labels,
-            ioconfig,
-            patch_mode=patch_mode,
-        )
-
-    def save_wsi_output(
-        self: EngineABC,
-        raw_output: dict,
-        save_dir: Path,
-        **kwargs: dict,
-    ) -> Path:
-        """Test post_process_wsi."""
-        return super().save_wsi_output(
-            raw_output,
-            save_dir=save_dir,
-            **kwargs,
-        )
-
-    def infer_wsi(
-        self: EngineABC,
-        dataloader: torch.utils.data.DataLoader,
-        save_path: Path,
-        **kwargs: dict,
-    ) -> dict | np.ndarray:
-        """Test infer_wsi."""
-        return super().infer_wsi(
-            dataloader,
-            save_path,
-            **kwargs,
-        )
 
 
 def test_engine_abc() -> NoReturn:
