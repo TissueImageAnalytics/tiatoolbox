d82cc3d91acb9be4c5402b6a28b22cdc5972e0e4
Jiaqi Lv <lvjiaqi9@gmail.com>
2025-10-28 15:22:20 +0000

    improve test coverage

--- tests/models/test_arch_grandqc.py
+++ tests/models/test_arch_grandqc.py
@@ -1,8 +1,5 @@
 """Unit test package for GrandQC Tissue Model."""
 
-from collections.abc import Callable
-from pathlib import Path
-
 import numpy as np
 import torch
 
@@ -13,12 +10,12 @@ from tiatoolbox.models.architecture import (
 from tiatoolbox.models.architecture.grandqc import TissueDetectionModel
 from tiatoolbox.models.engine.io_config import IOSegmentorConfig
 from tiatoolbox.utils.misc import select_device
-from tiatoolbox.wsicore.wsireader import WSIReader
+from tiatoolbox.wsicore.wsireader import VirtualWSIReader
 
 ON_GPU = False
 
 
-def test_functional_grandqc(remote_sample: Callable) -> None:
+def test_functional_grandqc() -> None:
     """Test for GrandQC model."""
     # test fetch pretrained weights
     pretrained_weights = fetch_pretrained_weights("grandqc_tissue_detection_mpp10")
@@ -40,9 +37,10 @@ def test_functional_grandqc(remote_sample: Callable) -> None:
     assert model.num_output_channels == 2
 
     # test inference
-    mini_wsi_svs = Path(remote_sample("wsi2_4k_4k_svs"))
-    reader = WSIReader.open(mini_wsi_svs)
-    read_kwargs = {"resolution": 10.0, "units": "mpp", "coord_space": "resolution"}
+    generator = np.random.default_rng(1337)
+    test_image = generator.integers(0, 256, size=(2048, 2048, 3), dtype=np.uint8)
+    reader = VirtualWSIReader.open(test_image)
+    read_kwargs = {"resolution": 0, "units": "level", "coord_space": "resolution"}
     batch = np.array(
         [
             reader.read_bounds((0, 0, 512, 512), **read_kwargs),
@@ -52,3 +50,21 @@ def test_functional_grandqc(remote_sample: Callable) -> None:
     batch = torch.from_numpy(batch)
     output = model.infer_batch(model, batch, device=select_device(on_gpu=ON_GPU))
     assert output.shape == (2, 512, 512, 2)
+
+
+def test_grandqc_preproc_postproc() -> None:
+    """Test GrandQC preproc and postproc functions."""
+    model = TissueDetectionModel(num_input_channels=3, num_output_channels=2)
+
+    generator = np.random.default_rng(1337)
+    # test preproc
+    dummy_image = generator.integers(0, 256, size=(512, 512, 3), dtype=np.uint8)
+    preproc_image = model.preproc(dummy_image)
+    assert preproc_image.shape == dummy_image.shape
+    assert preproc_image.dtype == np.float64
+
+    # test postproc
+    dummy_output = generator.random(size=(512, 512, 2), dtype=np.float32)
+    postproc_image = model.postproc(dummy_output)
+    assert postproc_image.shape == (512, 512)
+    assert postproc_image.dtype == np.int64
