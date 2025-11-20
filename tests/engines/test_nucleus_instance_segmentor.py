4bc33b724e5548a6da35199a09f149b4b0ce9940
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2025-11-20 10:59:13 +0000

    :test_tube: Test issues with raw output in patch mode

--- tests/engines/test_nucleus_instance_segmentor.py
+++ tests/engines/test_nucleus_instance_segmentor.py
@@ -3,26 +3,56 @@
 import shutil
 from collections.abc import Callable
 from pathlib import Path
+from typing import Literal, Final
 
 import torch
+import numpy as np
 
 from tiatoolbox.models import IOSegmentorConfig, NucleusInstanceSegmentor
+from tiatoolbox.wsicore import WSIReader
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
-def test_functionality_tile(source_image: Path, track_tmp_path: Path) -> None:
+def test_functionality_patch_mode(remote_sample: Callable, track_tmp_path: Path) -> None:
+    """Patch mode functionality test for nuclei instance segmentor."""
+    mini_wsi_svs = Path(remote_sample("wsi4_1k_1k_svs"))
+    mini_wsi = WSIReader.open(mini_wsi_svs)
+    size = (256, 256)
+    resolution = 0.25
+    units: Final = "mpp"
+    patch1 = mini_wsi.read_rect(
+        location=(0, 0),
+        size=size,
+        resolution=resolution,
+        units=units,
+    )
+    patch2 = mini_wsi.read_rect(
+        location=(512, 512),
+        size=size,
+        resolution=resolution,
+        units=units,
+    )
+
+    patches = np.stack(
+        arrays=[patch1, patch2],
+        axis=0
+    )
+
     inst_segmentor = NucleusInstanceSegmentor(
         batch_size=1,
         num_workers=0,
         model="hovernet_fast-pannuke",
     )
     output = inst_segmentor.run(
-        [source_image],
+        images=patches,
         patch_mode=True,
         device=device,
         save_dir=track_tmp_path / "hovernet_fast-pannuke",
+        output_type="dict",
     )
+
+    assert output
 
 
 def test_functionality_wsi(remote_sample: Callable, track_tmp_path: Path) -> None:
