a20ec2b5720f9681d9b4b3f2143486e4a24991e7
Jiaqi-Lv <60471431+Jiaqi-Lv@users.noreply.github.com>
2026-01-14 17:08:24 +0000

    🏗️ Update Remote Sample URLs in Notebooks and Files (#985)
    This pull request updates sample data URLs in some of the notebooks and all the tests to use the Hugging Face dataset repository instead of the previous TIA server. 
    
    **Migration of sample data URLs to Hugging Face:**
    
    * `examples/02-stain-normalization.ipynb` 
    *  `examples/03-tissue-masking.ipynb` 
    * `examples/04-patch-extraction.ipynb` 
    * `tests/test_utils.py`, `tests/test_wsireader.py`
    * Updated example usage in the docstring of `tiatoolbox/utils/tiff_to_fsspec.py` to use the new Hugging Face sample WSI URL.

--- tests/test_wsireader.py
+++ tests/test_wsireader.py
@@ -272,7 +272,7 @@ def fsspec_wsi(sample_svs: Path, track_tmp_path: Path) -> FsspecJsonWSIReader:
 
     since loading the whole image using HTTP range requests from:
 
-    https://tiatoolbox.dcs.warwick.ac.uk/sample_wsis/CMU-1-Small-Region.svs
+    https://huggingface.co/datasets/TIACentre/TIAToolBox_Remote_Samples/resolve/main/sample_wsis/CMU-1-Small-Region.svs
 
     takes about 20 seconds.
 
@@ -289,9 +289,7 @@ def fsspec_wsi(sample_svs: Path, track_tmp_path: Path) -> FsspecJsonWSIReader:
     )
     svs_file_path = str(files_all[0])
     json_file_path = str(track_tmp_path / "fsspec.json")
-    final_url = (
-        "https://tiatoolbox.dcs.warwick.ac.uk/sample_wsis/CMU-1-Small-Region.svs"
-    )
+    final_url = "https://huggingface.co/datasets/TIACentre/TIAToolBox_Remote_Samples/resolve/main/sample_wsis/CMU-1-Small-Region.svs"
     tiff_to_fsspec.main(svs_file_path, json_file_path, final_url)
 
     _FSSPEC_WSI_CACHE[cache_key] = wsireader.FsspecJsonWSIReader(json_file_path)
@@ -2979,7 +2977,7 @@ def test_fsspec_wsireader_info_read(sample_svs: Path, track_tmp_path: Path) -> N
 
     Generate fsspec json file and load image from:
 
-    https://tiatoolbox.dcs.warwick.ac.uk/sample_wsis/CMU-1-Small-Region.svs
+    https://huggingface.co/datasets/TIACentre/TIAToolBox_Remote_Samples/resolve/main/sample_wsis/CMU-1-Small-Region.svs
 
     """
     wsi = fsspec_wsi(sample_svs, track_tmp_path)
