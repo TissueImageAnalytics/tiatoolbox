06c041f68b6281952a0278d619a601fca57c570b
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2026-03-12 12:15:48 +0000

    :bug: Fix setup.py to include 3.14

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ test_requirements = [
 setup(
     author="TIA Centre",
     author_email="TIA@warwick.ac.uk",
-    python_requires=">=3.11, <=3.14",
+    python_requires=">=3.11, <3.15",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
