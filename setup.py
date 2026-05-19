79620f7a14b43d52ebe0a851a10b27d279184796
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2026-05-19 09:44:00 +0100

    :pushpin: Bump version to `2.1.0`

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/TissueImageAnalytics/tiatoolbox",
-    version="2.0.1",
+    version="2.1.0",
     zip_safe=False,
 )
