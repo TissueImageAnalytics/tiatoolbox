0202a7d039a9f0eabb3d0afc5a9254fe3807200b
Aakash Madhav Rao <axr1154@student.bham.ac.uk>
2026-05-28 16:55:24 +0100

    :bug: Remove `ignore_index` for `fcn_resnet50_unet-bcss` (#1069)
    In fcn_resnet50_unet-bcss ignore_index is defined as 0 which corresponds to the tumor class which leads to exclusion of tumor class during annotation store writing. 
    
    Addresses #1068 
    
    ---------
    
    Co-authored-by: Jiaqi Lv <lvjiaqi9@gmail.com>

--- tiatoolbox/models/architecture/efficientunet_tissue_mask_model.py
+++ tiatoolbox/models/architecture/efficientunet_tissue_mask_model.py
@@ -780,6 +780,7 @@ class EfficientUNetTissueMaskModel(ModelABC):
         self: EfficientUNetTissueMaskModel,
         num_classes: int = 1,
         threshold: float = 0.95,
+        class_dict: dict | None = None,
     ) -> None:
         """Initialize EfficientUNetTissueMaskModel.
 
@@ -791,6 +792,8 @@ class EfficientUNetTissueMaskModel(ModelABC):
                 Number of output classes. Defaults to 1 (binary segmentation).
             threshold (float):
                 Threshold for binary segmentation. Defaults to 0.95.
+            class_dict (dict | None):
+                Optional dictionary mapping class indices to names. Defaults to None.
 
         """
         super().__init__()
@@ -798,6 +801,7 @@ class EfficientUNetTissueMaskModel(ModelABC):
         self.decoder = UnetDecoder()
         self.segmentation_head = SegmentationHead(16, num_classes)
         self.threshold = threshold
+        self.class_dict = class_dict
 
     def forward(  # skipcq: PYL-W0613
         self: EfficientUNetTissueMaskModel,
--- tiatoolbox/models/architecture/unet.py
+++ tiatoolbox/models/architecture/unet.py
@@ -273,6 +273,8 @@ class UNetModel(ModelABC):
             Choosing between "add" or "concat" method to be used for
             combining feature maps from encoder and decoder parts at
             skip connections. Default is "add".
+        class_dict (dict | None):
+                Optional dictionary mapping class indices to names. Defaults to None.
 
     Returns:
         torch.nn.Module:
@@ -297,9 +299,12 @@ class UNetModel(ModelABC):
         encoder_levels: list[int] | None = None,
         decoder_block: tuple[int] | None = None,
         skip_type: str = "add",
+        class_dict: dict | None = None,
     ) -> None:
         """Initialize :class:`UNetModel`."""
         super().__init__()
+
+        self.class_dict = class_dict
 
         if encoder.lower() not in {"resnet50", "unet"}:
             msg = f"Unknown encoder `{encoder}`"
