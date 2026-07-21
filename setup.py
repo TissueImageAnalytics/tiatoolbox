68bf04ca0c1ddd7a19bf4bd3ce8acf62c8d0c69f
Shan E Ahmed Raza <13048456+shaneahmed@users.noreply.github.com>
2026-07-21 16:18:57 +0100

    🔖 Release 2.1.3 (#1098)
    ## TIAToolbox v2.1.3 (2026-07-21)
    
    ### 🐞 Bug Fixes and Other Changes
    
    - Fix DFBR feature mapping test (#1090)
    - Fix padded contours for Visualization (#1095)
    - Fix handling of `None` `return_predictions` in `NucleusInstanceSegmentor` CLI (#1089), contributed by @Anannya30
    
    ### 🛠️ Development-Related Changes
    
    - Remove `albumentations` dependencies (#1086)
      - The alternative `albumentationsx` has incompatible license. Therefore, we do not plan to include this in future release.
    - Refactor `wsireader` #1088
      - `wsireader` will be restructured for maintainability in future releases without breaking changes to the API.
    - Fix `tifffile` and `zarr` min versions on Colab (#1091)
      - Python `3.11` will be dropped in future releases for compatibility with latest releases of `zarr`.
    - Remove Unnecessary Workflows on push (#1094)
    
    **Full Changelog:** https://github.com/TissueImageAnalytics/tiatoolbox/compare/v2.1.2...v2.1.3

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/TissueImageAnalytics/tiatoolbox",
-    version="2.1.2",
+    version="2.1.3",
     zip_safe=False,
 )
