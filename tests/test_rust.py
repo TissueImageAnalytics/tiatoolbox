4e033c1f2baf305540e9190618a78e12401e9982
Hannah Wood <hannah.wood.2@warwick.ac.uk>
2026-08-25 19:13:01 +0100

    Updated tests

--- tests/test_rust.py
+++ tests/test_rust.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from tiatoolbox import utils
+from tiatoolbox import rmisc, utils
 
 
 def test_contrast_enhancer() -> None:
@@ -61,3 +61,88 @@ def test_contrast_enhancer() -> None:
     output_array = utils.misc.contrast_enhancer(input_array, low_p=2, high_p=98)
     # The out_put array should be equal to expected result_array
     assert np.all(result_array == output_array)
+
+
+def test_patch_predictions_as_qupath_json() -> None:
+    """Tests that the rust code for patch_predictions_as_qupath_json works correctly."""
+    class_colours = {
+        0.0: [255, 0, 0],
+        1.0: [0, 255, 0],
+    }
+
+    class_dict = {
+        0.0: "Tumour",
+        1.0: "Normal",
+    }
+
+    preds = [0.0, 1.0]
+
+    patch_coords = np.array(
+        [
+            [10.0, 20.0, 30.0, 40.0],
+            [50.0, 60.0, 70.0, 80.0],
+        ],
+        dtype=np.float64,
+    )
+
+    result = rmisc.patch_predictions_as_qupath_json(
+        class_colours,
+        preds,
+        class_dict,
+        patch_coords,
+    )
+
+    expected = [
+        {
+            "type": "Feature",
+            "id": "patch_0",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": (
+                    (
+                        (10.0, 20.0),
+                        (10.0, 40.0),
+                        (30.0, 40.0),
+                        (30.0, 20.0),
+                        (10.0, 20.0),
+                    ),
+                ),
+            },
+            "properties": {
+                "classification": {
+                    "name": "Tumour",
+                    "color": [255, 0, 0],
+                }
+            },
+            "objectType": "annotation",
+            "name": "Tumour",
+            "class_value": 0.0,
+        },
+        {
+            "type": "Feature",
+            "id": "patch_1",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": (
+                    (
+                        (50.0, 60.0),
+                        (50.0, 80.0),
+                        (70.0, 80.0),
+                        (70.0, 60.0),
+                        (50.0, 60.0),
+                    ),
+                ),
+            },
+            "properties": {
+                "classification": {
+                    "name": "Normal",
+                    "color": [0, 255, 0],
+                }
+            },
+            "objectType": "annotation",
+            "name": "Normal",
+            "class_value": 1.0,
+        },
+    ]
+
+    assert result == expected
